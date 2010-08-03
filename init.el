5dd5cc4ede376b765ded3260cbda3b1d4e0b6c5f
unknown <Administrator@.(none)>
2010-08-03 13:59:17 +0800

    init

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; -*-no-byte-compile: t; -*-
 ;;{{{ 时间戳
-;;;;Time-stamp: <Joseph 2011-08-01 20:59:10 星期一>
+;;;;Time-stamp: <Joseph 2010-08-03 13:59:03 星期二>
 ;;}}}
 ;;  ╭∩╮⎝▓▓⎠╭∩╮
 ;; ▇█▓▒░◕~◕░▒▓█▇
@@ -70,6 +70,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(global-semantic-tag-folding-mode t nil (semantic-util-modes))
-  '(semantic-idle-scheduler-idle-time 3)
- '(semantic-self-insert-show-completion-function (lambda nil (semantic-ia-complete-symbol-menu (point)))))
+ )
