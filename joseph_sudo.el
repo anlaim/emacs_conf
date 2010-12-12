a3e8d27234ee6fdfbba53f772772fb4103ca84af
jixiuf <jixiuf@gmail.com>
2010-12-13 00:09:54 +0800

    merge joseph_sudo.el

--- joseph_sudo.el
+++ joseph_sudo.el
@@ -1,51 +1,48 @@
 ;;;;;;;;;;;; this stuff help you to switch between edit current buffer as root and as  common user
 ;;;;;;;;;;;(global-set-key (kbd "C-c o") 'toggle-read-only-file-with-sudo)
 ;;;;;;;;;;;; also you can  /usr/bin/emacsclient -t -e "(wl-sudo-find-file \"$1\" \"$PWD\")" 
-;(global-set-key (kbd "C-c o") 'toggle-read-only-file-with-sudo)
-(defvar toggle-to-root-hook nil
-  "Normal hook for functions to run after toggle to \"root\" user to edit this file ")
-(defvar hostname "localhost")   ;;default is localhost ,you may set hostname to your hostname
-(defvar toggle-to-common-user-hook nil
-  "Normal hook for functions to run after toggle back to read-only state")
-  
+;;{{{ toggle-read-only-file-with-sudo  函数的定义
 (defun toggle-read-only-file-with-sudo ()
   (interactive)
-  (let ((fname (or buffer-file-name
-                   dired-directory)))
-(shell-command (concat "logger before " fname)  )
+  (let ((fname (or buffer-file-name dired-directory) )
+        (hostname (shell-command-to-string "hostname" ))
+        (hostname_without_newline nil)
+        )
+    (setq hostname_without_newline  (substring hostname  0 (string-match "$" hostname )) ) 
     (when fname
-      (if (string-match "^/sudo:" fname) 
-            (setq fname  (replace-regexp-in-string  (concat  "^/sudo:root@" hostname  ":" )  ""  (replace-regexp-in-string "^/sudo:root@localhost:"   "" fname) ) )
-                (setq fname (concat "/sudo:root@localhost:"  fname))
+      (if (string-match "^/sudo:" fname) ;;根据是不是以/sudo开头切换用不用/sudo::打开文件
+          (setq fname  (replace-regexp-in-string  (concat  "^/sudo:root@" hostname_without_newline ":" )  ""  (replace-regexp-in-string "^/sudo:root@localhost:"   "" fname) ) )
+        (setq fname (concat "/sudo:root@localhost:"  fname))
         )
       (find-alternate-file fname) ;;this is the function 
-      (if  (string-match "^/sudo:" fname)
-          (run-hooks 'toggle-to-root-hook) 
-          (run-hooks 'toggle-to-common-user-hook) 
-        )
-      
       )))
 
 (defun wl-sudo-find-file (file dir)
   (find-file (concat "/sudo:root@localhost:" (expand-file-name file dir)))
-          (run-hooks 'toggle-to-root-hook) 
+  )
+;;}}}
 
-)
-
+;;{{{ 当切换到root 用户时，为作区别 ，外观显红色
 (defface toggle-to-root-header-face
   '((t (:foreground "white" :background "red3")))
   "*Face use to display header-lines for files opened as root.")
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; define a funtion ,and link it to hook toggle-to-root-hook
+;;when open a file with sudo ,then change the face of the file to waring
 (defun toggle-to-root-header-warning ()
   "*Display a warning in header line of the current buffer.
    This function is suitable to add to `toggle-to-root-hook'."
   (let* ((warning "WARNING: EDITING FILE AS ROOT!")
-	 (space (+ 6 (- (window-width) (length warning))))
-	 (bracket (make-string (/ space 5) ?-))
-	 (warning (concat bracket bracket warning bracket bracket bracket )))
+         (space (+ 6 (- (window-width) (length warning))))
+         (bracket (make-string (/ space 5) ?-))
+         (warning (concat bracket bracket warning bracket bracket bracket )))
     (setq header-line-format
-	  (propertize  warning 'face 'toggle-to-root-header-face))))
-(add-hook 'toggle-to-root-hook 'toggle-to-root-header-warning)
+          (propertize  warning 'face 'toggle-to-root-header-face))))
+;;}}}
 
-;(remove-hook 'toggle-to-root-hook 'toggle-to-root-header-warning)
-(provide 'jixiuf_sudo)
+;;{{{ 加载一个新文件时，如果是sudo 开头的文件 ，也加上红色的外观
+(defun my_find_file_hook ()
+  (if  (string-match "^/sudo:" (buffer-file-name)) (toggle-to-root-header-warning) )
+  )
+(add-hook 'find-file-hooks      'my_find_file_hook);; find-file-hooks 是加载完file 之后调用的一个hook 
+;;}}}
+
+(provide 'joseph_sudo)
