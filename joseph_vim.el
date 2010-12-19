3466c817170a6a4f4206cf8bca79604dea37b3de
jixiuf <jixiuf@gmail.com>
2010-12-19 22:24:06 +0800

    modify function (joseph-go-to-char )

--- joseph_vim.el
+++ joseph_vim.el
@@ -1,12 +1,18 @@
-;;{{{     相当于vi 中的o命令，在后面插入一新行，并移动光标到新行
-(global-set-key (kbd "C-j") 'open-and-move-to-next-line);
-(defun open-and-move-to-next-line(&optional arg)
-  (interactive "p")
-    (end-of-line)
-    (open-line arg)
-    (next-line)
-    (indent-according-to-mode)
-    )    
+;;; joseph_vim.el --- config about some vim feathers
+;;{{{ 时间戳
+;;;;Time-stamp: <jixiuf 2010-12-19 22:23:01>
+;;}}}
+
+;;{{{     相当于vi 中的o命令，在下面插入一新行，并移动光标到新行
+;;熟悉了emacs 后,不再使用 C-e C-j就可以实现
+;; (global-set-key (kbd "C-j") 'open-and-move-to-next-line);
+;; (defun open-and-move-to-next-line(&optional arg)
+;;   (interactive "p")
+;;     (end-of-line)
+;;     (open-line arg)
+;;     (next-line)
+;;     (indent-according-to-mode)
+;;     )    
     
 ;;}}}
 
@@ -22,10 +28,9 @@
   )
 ;;}}}
 
-(setq-default kill-whole-line t) ;; 在行首 C-k 时，同时删除该行
-;;;;剪切当前行相对简单C-k 为剪切光标后的字符，C-a C-k 则为剪切当前行了
 
 ;;{{{ 不在需要的copy当前行 ()
+;;joseph_clipboard_and_encoding.el中有关于copy当前行的更好的配置
 ;; (global-set-key (kbd "C-c C-k") 'copy-lines);
 ;; (defun copy-lines(&optional arg)
 ;;     (interactive "p")
@@ -40,9 +45,9 @@
 ;;}}}
 
 ;;{{{ ;合并当前行与下一行，同vim的 J命令 ,并作了增强，可以合并多行，使用方法 C-u n C-c C-j ;n是次数:
-(global-set-key (kbd "C-c C-j") 'join-lines)
-(global-set-key (kbd "C-x C-j") 'join-lines)
-(defun join-lines(&optional arg)
+(global-set-key (kbd "C-c C-j") 'joseph-join-lines)
+;(global-set-key (kbd "C-x C-j") 'joseph-join-lines)
+(defun joseph-join-lines(&optional arg)
   (interactive "*p")
     (save-excursion
     (let ((index 1)   )        ;index 初始化为0
@@ -57,6 +62,7 @@
   )
 )
 ;;}}}
+
 ;;{{{;;像vi一样用%在匹配的括号间跳转
 (global-set-key "%" 'match-paren)
 (defun match-paren (arg)
@@ -68,33 +74,38 @@
 	((looking-at "\\s\}") (forward-char 1) (backward-list 1))
 	(t (self-insert-command (or arg 1)))))
 ;;}}}
+
 ;;{{{ ;此函数可以进行快速定位 ,vi 中有个f命令如fa 搜索a 并跳到相应位置, 如果这个函数用熟了完全可以去掉C-f 与C-b这两个键
+
 ;; 而这个命令与之相似，如将命令绑到C-f后，按下C-f后 连续按一个字母如s则会一直搜索s 并定位到相应的位置，按C-h可反向搜索
 ;;直到按下不同的字母(源码在王垠的wy-go-tochar上进行了修改，可以进行反向搜索)
 ;;郑重向大家推荐我写的 ,把它绑定到C-f ,它具有普通C-f 向前移到一个字符的功能,同时又能根据特定字符快速定位
 (defun joseph-go-to-char (n )
   "Move forward to Nth occurence of CHAR.
 Typing `joseph-go-to-char-key' again will move forwad to the next Nth
-occurence of CHAR. Typing Backspace will move back . "
+occurence of CHAR. Typing \C-h will move back ."
   (interactive "p")
-  (forward-char 1)
-  (let( (char (read-char "Go to char:" ) ))
-  (message (string char) )
-  (if (string-equal "" (string char) )
-      (forward-char 1)
-    (unless   ( char-equal ?  char)
-         (search-forward (string char) nil nil n)
-               (let ((readed-char (read-char "go to next position" ))  )       
-                 (while (or (char-equal readed-char char)  ;;it equals to the original inputed char ,then search it again
-                            (char-equal ? readed-char )  ;; Ctrl+h     means   search back for the char
-                            )
-                   (if    (char-equal ? readed-char);;C-h backward search
-                           (search-backward (string char) nil nil n   )                         
-                           (search-forward (string char) nil nil n) )
-                 (setq readed-char (read-char "C-h for backward search ,same char for forward search")) )
-                 (setq unread-command-events (list last-input-event)) ) 
-      ) ) ) )
+  (forward-char n)
+  (let( (char (read-char "Go to Char:" ) ))
+  (if (string-match "[[:cntrl:]]" (string char) )
+      (setq unread-command-events (list last-input-event))
+      (progn    (message (string char) )
+        (search-forward (string char) nil nil n)
+        (let ( ( readed-char (read-char
+                (concat "?\C-h for backward search ,\"" (string char)   "\" for forward search:")) )   )
+          (while (or (char-equal readed-char char)  
+                     (char-equal ?\C-h readed-char ) )
+            (if    (char-equal ?\C-h readed-char)
+                (search-backward (string char) nil nil n   )
+              (search-forward (string char) nil nil n) )
+            (setq readed-char (read-char
+                  (concat "?\C-h for backward search ,\"" (string char)   "\" for forward search:")) ))
+          (setq unread-command-events (list last-input-event)) )
+        ) 
+    )
+  ) )
 (define-key global-map (kbd "C-f") 'joseph-go-to-char)
 ;;}}}
 (provide 'joseph_vim)
 
+;;; joseph_vim.el ends here
