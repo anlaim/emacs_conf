d44776acbb58f00ba2e8c8897f2f20503e3944b8
jixiuf <jixiuf@gmail.com>
2011-04-03 15:21:00 +0800

    rm unused/*

--- site-lisp/unused/elpa/company-0.5/company-my-frontend.el
+++
@@ -1,7 +0,0 @@
- (defun company-my-backend (command &optional arg &rest ignored)
-    (case command
-      ('prefix (when (looking-back "foo\\>")
-                 (match-string 0)))
-      ('candidates (when (equal arg "foo")
-                      (list "foobar" "foobaz" "foobarbaz")))
-      ('meta (format "This value is named %s" arg))))
