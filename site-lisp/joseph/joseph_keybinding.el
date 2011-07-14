90c300c8efa8d25f922ad262e4f0dc4bfa0dbf95
jixf.softbrain <jixiuf@gmail.com>
2011-07-14 16:29:15 +0800

    C-a

--- site-lisp/joseph/joseph_keybinding.el
+++ site-lisp/joseph/joseph_keybinding.el
@@ -93,6 +93,7 @@
 
 (global-set-key "\C-j" 'open-line-or-new-line-dep-pos)
 (global-set-key (kbd "C-q") 'smart-beginning-of-line)
+(global-set-key (kbd "C-a") 'smart-beginning-of-line)
 (global-set-key (kbd "C-e") 'smart-end-of-line)
 ;;(global-set-key (kbd "C-a" ) (quote  quoted-insert))
 (global-set-key (kbd "C-c C-j") 'joseph-join-lines)
