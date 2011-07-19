fca7582dbf1594c9b57dbf7428717da43b034a08
jixiuf <jixiuf@gmail.com>
2011-07-20 00:41:44 +0800

    load anything after icicle

--- site-lisp/joseph/joseph-anything.el
+++ site-lisp/joseph/joseph-anything.el
@@ -9,7 +9,7 @@
 (autoload 'descbinds-anything "descbinds-anything")
 (fset 'describe-bindings 'descbinds-anything)
 
-(run-with-idle-timer 5 nil '(lambda () (require 'anything) (message "anything.el is loaded")))
+(run-with-idle-timer 8 nil '(lambda () (require 'anything) (message "anything.el is loaded")))
 
 ;;(require 'anything-config)
 (eval-after-load 'anything
