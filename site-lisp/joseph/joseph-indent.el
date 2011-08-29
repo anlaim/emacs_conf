c44f2174e6b449d88c7ee481df99d664a7775231
jixiuf <jixiuf@gmail.com>
2011-08-29 19:10:09 +0800

    add

---
+++ site-lisp/joseph/joseph-indent.el
@@ -0,0 +1,69 @@
+;;; joseph-indent.el --- indent    -*- coding:utf-8 -*-
+
+;; Description: indent
+;; Time-stamp: <Joseph 2011-08-29 19:09:56 星期一>
+;; Created: 2011-08-29 19:09
+;; Author: 孤峰独秀  jixiuf@gmail.com
+;; Maintainer:  孤峰独秀  jixiuf@gmail.com
+;; Keywords:indent
+;; URL: http://www.emacswiki.org/emacs/joseph-indent.el
+
+;; Copyright (C) 2011, 孤峰独秀, all rights reserved.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Commands:
+;;
+;; Below are complete command list:
+;;
+;;
+;;; Customizable Options:
+;;
+;; Below are customizable option list:
+;;
+
+;;; Code:
+
+
+;;; 粘贴的时候自动缩进
+;; automatically indenting yanked text if in programming-modes
+(defvar yank-indent-modes '(emacs-lisp-mode lisp-mode
+                                            c-mode c++-mode js2-mode
+                                            tcl-mode sql-mode
+                                            perl-mode cperl-mode
+                                            java-mode jde-mode
+                                            lisp-interaction-mode
+                                            LaTeX-mode TeX-mode
+                                            scheme-mode clojure-mode)
+  "Modes in which to indent regions that are yanked (or yank-popped)")
+
+(defadvice yank (after indent-region activate)
+  "If current mode is one of 'yank-indent-modes, indent yanked text (with prefix arg don't indent)."
+  (if (member major-mode yank-indent-modes)
+      (let ((mark-even-if-inactive t))
+        (indent-region (region-beginning) (region-end) nil))))
+
+(defadvice yank-pop (after indent-region activate)
+  "If current mode is one of 'yank-indent-modes, indent yanked text (with prefix arg don't indent)."
+  (if (member major-mode yank-indent-modes)
+      (let ((mark-even-if-inactive t))
+        (indent-region (region-beginning) (region-end) nil))))
+
+
+(provide 'joseph-indent)
+;;; joseph-indent.el ends here
