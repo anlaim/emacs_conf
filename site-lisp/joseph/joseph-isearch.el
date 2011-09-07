c84933239ac83bdda5cca0b9504de911e789e389
jixiuf <jixiuf@gmail.com>
2011-09-08 00:54:25 +0800

    isearch

---
+++ site-lisp/joseph/joseph-isearch.el
@@ -0,0 +1,100 @@
+;;; joseph-isearch.el --- isearch custom   -*- coding:utf-8 -*-
+
+;; Description: isearch custom
+;; Time-stamp: <Joseph 2011-09-08 00:53:22 星期四>
+;; Created: 2011-09-08 00:42
+;; Author: 孤峰独秀  jixiuf@gmail.com
+;; Maintainer:  孤峰独秀  jixiuf@gmail.com
+;; Keywords:isearch
+;; URL: http://www.emacswiki.org/emacs/joseph-isearch.el
+
+;; Copyright (C) 2011, 孤峰独秀, all rights reserved.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Commands:
+;;
+;; Below are complete command list:
+;;
+;;  `my-anything-occur'
+;;    Preconfigured Anything for Occur source.
+;;  `isearch-yank-symbol'
+;;    *Put symbol at current point into search string.
+;;
+;;; Customizable Options:
+;;
+;; Below are customizable option list:
+;;
+
+;;; Code:
+
+;; Use regex searching by default
+(global-set-key "\C-s" 'isearch-forward-regexp)
+(global-set-key "\C-r" 'isearch-backward-regexp)
+(global-set-key "\C-\M-s" 'isearch-forward)
+(global-set-key "\C-\M-r" 'isearch-backward)
+
+(defun call-with-current-isearch-string-as-regex (f)
+  (let ((case-fold-search isearch-case-fold-search))
+    (funcall f (if isearch-regexp isearch-string (regexp-quote isearch-string)))))
+
+
+(defun my-anything-occur (regexp)
+  "Preconfigured Anything for Occur source.
+If region is active, search only in region,
+otherwise search in whole buffer."
+  (interactive)
+  (let ((anything-compile-source-functions
+         ;; rule out anything-match-plugin because the input is one regexp.
+         (delq 'anything-compile-source--match-plugin
+               (copy-sequence anything-compile-source-functions)))
+        (anything-samewindow nil))
+    (anything 'anything-c-source-occur regexp  "Regexp:"  "*Anything Occur*")
+    ))
+;; Activate occur easily inside isearch
+(define-key isearch-mode-map (kbd "C-o")
+  (lambda ()
+    (interactive)
+    (call-with-current-isearch-string-as-regex 'my-anything-occur)))
+
+;; or fire up "all"
+(define-key isearch-mode-map (kbd "C-l")
+  (lambda ()
+    (interactive)
+    (call-with-current-isearch-string-as-regex 'all)))
+
+;; Search back/forth for the symbol at point
+;; See http://www.emacswiki.org/emacs/SearchAtPoint
+(defun isearch-yank-symbol ()
+  "*Put symbol at current point into search string."
+  (interactive)
+  (let ((sym (symbol-at-point)))
+    (if sym
+        (progn
+          (setq isearch-regexp t
+                isearch-string (concat "\\_<" (regexp-quote (symbol-name sym)) "\\_>")
+                isearch-message (mapconcat 'isearch-text-char-description isearch-string "")
+                isearch-yank-flag t))
+      (ding)))
+  (isearch-search-and-update))
+
+(define-key isearch-mode-map "\C-\M-w" 'isearch-yank-symbol)
+
+
+(provide 'joseph-isearch)
+;;; joseph-isearch.el ends here
--- site-lisp/joseph/joseph_init.el
+++ site-lisp/joseph/joseph_init.el
@@ -1,5 +1,5 @@
 ;;; -*- coding:utf-8 -*-
-;; Time-stamp: <Joseph 2011-09-08 00:37:38 星期四>
+;; Time-stamp: <Joseph 2011-09-08 00:54:08 星期四>
 ;;; byte compile
 (eval-when-compile
     (add-to-list 'load-path  (expand-file-name "."))
@@ -42,7 +42,8 @@
 (require 'joseph-autopair-config)
 (require 'joseph-highlight-parentheses)
 (require 'joseph-icicle) ;  icicle
-((require 'joseph-iedit )
+(require 'joseph-iedit )
+(require 'joseph-isearch )
 (run-with-idle-timer 10 nil '(lambda () (require 'joseph-cedet) (message "cedet is loaded")));;;  cedet
 (eval-after-load 'shell '(require 'joseph-shell));;; shell
 (eval-after-load 'sql '(require 'joseph-sql));;; Sql
