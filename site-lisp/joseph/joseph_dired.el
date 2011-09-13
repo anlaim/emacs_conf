18779178c4381121df132fbc0861260b865da7df
jixiuf <jixiuf@gmail.com>
2011-09-13 22:39:52 +0800

    dired add to load path or load it

--- site-lisp/joseph/joseph_dired.el
+++ site-lisp/joseph/joseph_dired.el
@@ -268,5 +268,20 @@
 (eval-after-load 'dired '(progn (require 'joseph-single-dired)))
 (eval-after-load 'dired '(progn (require 'dired-filetype-face)))
 
+(defun dired-add-to-load-path-or-load-it()
+  "on `dired-mode',if thing under point is directory add it to `load-path'
+if it is a el-file ,then `load' it"
+  (interactive)
+  (let ((dir-or-file  (dired-get-filename)))
+    (if (file-directory-p dir-or-file)
+        (progn
+        (add-to-list 'load-path dir-or-file)
+        (message (concat dir-or-file " added to load-path" ))
+        )
+      (if (string-equal  (file-name-extension  dir-or-file ) "el")
+          (load  dir-or-file)
+        (message (concat dir-or-file "is loaded"))
+        ))))
+(define-key-lazy dired-mode-map "L" 'dired-add-to-load-path-or-load-it 'dired)
 (provide 'joseph_dired)
 
