9b202fe420192133a5410662b4817d25dbe3ee46
jixiuf <jixiuf@gmail.com>
2011-10-23 20:48:14 +0800

    (defun dired-ediff(&optional arg)

--- site-lisp/joseph/joseph-dired-lazy.el
+++ site-lisp/joseph/joseph-dired-lazy.el
@@ -67,5 +67,34 @@ if it is a el-file ,then `load' it"
           (load  dir-or-file)
         (message (concat dir-or-file "is loaded"))
         ))))
+
+;;;###autoload
+(defun dired-ediff(&optional arg)
+  "在dired buffer中,如果mark了两个文件,则对此二文件进行diff
+,如果仅mark了一个文件,则将其作为其中之一,另一个文件名则让用户选择,默认是光标下的文件
+,如果没有mark任何文件,以当前文件作其一,另一,让用户选择.
+默认使用ediff进行比较,`C-u'则使用diff"
+  (interactive "P")
+  (let ((marked-file-or-cur-file (dired-get-marked-files nil nil nil t))
+        first , second)
+    (cond
+     ((and (= 2  (length marked-file-or-cur-file))
+           (not (eq t (car marked-file-or-cur-file))));;mark 2 files
+      (setq first (car marked-file-or-cur-file ))
+      (setq second (nth 1 marked-file-or-cur-file )))
+     ((and (= 2  (length marked-file-or-cur-file))
+           (eq t (car marked-file-or-cur-file)));;mark 1 file
+      (setq first (nth 1 marked-file-or-cur-file ))
+      (setq second (read-file-name "Diff Marked file with(default:current file):"
+                                   default-directory (dired-get-filename))))
+     ((and (= 1 (length marked-file-or-cur-file))
+           (not (file-directory-p (car marked-file-or-cur-file ))))
+      (setq first (car marked-file-or-cur-file ));;no mark file
+      (setq second (read-file-name "Diff current file with:" default-directory ))
+      ))
+    (if arg (progn (switch-to-buffer (diff first second))
+                   (delete-other-windows))
+      (ediff first second))))
+
 (provide 'joseph-dired-lazy)
 ;;; joseph-dired-lazy.el ends here
