8080492ac673285e7ff0cb1b36fdf37885a3db3d
jixiuf <jixiuf@gmail.com>
2011-10-28 14:28:36 +0800

    move code

--- site-lisp/joseph/joseph-w32.el
+++ site-lisp/joseph/joseph-w32.el
@@ -1,52 +1,9 @@
-;;; -*- coding:utf-8 -*-
+;; -*- coding:utf-8 -*-
 (setenv "HOME" "d:/")
-;; (setq process-coding-system-alist
-;;       (cons '("bash" . (raw-text-dos . raw-text-unix)) process-coding-system-alist))
-(setq process-coding-system-alist (cons '("bash" . undecided-unix) process-coding-system-alist))
 
 ;;now use bash as my shell ,
 ;; you can call (set-shell-cmdproxy )here to use cmdproxy as the shell.
-(eval-after-load 'shell
-'(progn
-;;;###autoload
-   (defun set-shell-bash()
-     "Enable on-the-fly switching between the bash shell and DOS."
-     (interactive)
-     ;; (setq binary-process-input t)
-     (setq comint-scroll-show-maximum-output 'this)
-     (setq shell-file-name "bash")
-     (setq shell-command-switch "-c")      ; SHOULD IT BE (setq shell-command-switch "-ic")?
-     (setq explicit-shell-file-name "bash") ;;term.el
-     (setenv "SHELL" explicit-shell-file-name)
-     (setq explicit-bash-args '("-login" "-i"))
-     (make-variable-buffer-local 'comint-completion-addsuffix)
-     (setq comint-completion-addsuffix t);;目录补全时,在末尾加一个"/"字符
-     (setq comint-eol-on-send t)
-     (setq comint-file-name-quote-list '(?\  ?\")) ;;当文件名中有这些(空格引号)特殊字符时会把这些特殊字符用"\"转义
-     (setq w32-quote-process-args ?\")  ;;再给程序传递参数的时候,使用这个字符将参数括起来
-     (eval-after-load 'ediff-diff '(progn (setq ediff-shell shell-file-name))) ;;Ediff shell
-     (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m nil t)
-     ;; Unfortunately, when you visit a DOS text file within an
-     ;; encoded file, you'll see CRs (^Ms) in the buffer.
-     ;; If `binary-process-output' is set to `nil', this problem goes
-     ;; away, which is fine for files of type `.gz'.
-     (setq binary-process-input t)
-     (setq binary-process-output nil)
-     )
-
-;;;###autoload
-   (defun set-shell-cmdproxy()
-     "Set shell to `cmdproxy'."
-     (interactive)
-     (setq shell-file-name "cmdproxy")
-     (setq explicit-shell-file-name "cmdproxy")
-     (setenv "SHELL" explicit-shell-file-name)
-  ;;;;;(setq explicit-sh-args nil)           ; Undefined?
-     (setq w32-quote-process-args nil))
-  (set-shell-bash)
-  (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-  ))
-
+;; (set-shell-bash)
 
 ;; ;;(setq exec-path (cons "C:/cygwin/bin" exec-path))
 ;; ;;(setq shell-file-name "C:/cygwin/bin/bash.exe") ; Subprocesses invoked via the shell.
@@ -55,25 +12,23 @@
 ;; ;;(setq explicit-shell-file-name shell-file-name) ; Interactive shell
 
 
-
-
-
-;; ;;dired 使用外部的ls 程序
+;;; ;;dired 使用外部的ls 程序
 (setq ls-lisp-use-insert-directory-program t)      ;; use external ls
 (setq insert-directory-program "ls") ;; ls program name
 
 ;;;dired 下,"Z" 无法使用gunzip 解压文件,原因是gunzip 是一个指向gzip的软链接,
-(eval-after-load 'dired-aux
-  '(progn
-     (defadvice dired-check-process (around gunzip-msys-symlink activate)
-       "on msys ,the gunzip is a symlink to gzip ,and dired can't
+(defadvice dired-check-process (around gunzip-msys-symlink activate)
+  "on msys ,the gunzip is a symlink to gzip ,and dired can't
         fine gunzip.exe to uncompress,this advice ,replace gunzip
         with gzip -d"
-       (when (string-equal program "gunzip")
-         (setq program "gzip")
-         (add-to-list 'arguments "-d"))
-       ad-do-it
-       )))
+  (when (string-equal program "gunzip")
+    (setq program "gzip")
+    (add-to-list 'arguments "-d"))
+  ad-do-it
+  )
+;; (eval-after-load 'dired-aux
+;;   '(progn
+;;      ))
 
 
 ;; (global-set-key (kbd "C-x C-b") 'joseph-hide-frame)
