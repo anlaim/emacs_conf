b59af6a3eae988a460cd35e0dde5d5f26b361bc4
jixiuf <jixiuf@gmail.com>
2011-10-28 14:15:12 +0800

    (apply 'start-process ;; byte comp

--- site-lisp/joseph/joseph-byte-compile.el
+++ site-lisp/joseph/joseph-byte-compile.el
@@ -8,11 +8,16 @@ or a simple file ,前提是emacs.exe emacs 在$PATH路径下"
     (if (stringp files) (setq file-strings files)
       (setq file-strings (mapconcat 'identity files " ")))
 
-    (setq process (start-process-shell-command
-                   "byte compile all my el files" "*joseph-byte-compiles-all*"
-                   (format "emacs -batch -l %s -f batch-byte-compile %s "
-                           (expand-file-name "~/.emacs.d/site-lisp/joseph/joseph_byte_compile_include.el")
-                           file-strings)))
+    (setq process
+          (apply 'start-process ;;
+                 "compile my el"
+                 (concat "*byte-compiles-all*-" (number-to-string (random)))
+                 "emacs"
+                 (apply 'list "-batch"
+                        "-l" (expand-file-name "~/.emacs.d/site-lisp/joseph/joseph_byte_compile_include.el")
+                        "-f" "batch-byte-compile"
+                        files))
+          )
     (set-process-sentinel process
                           (lambda (proc change)
                             (when (string-match "\\(finished\\|exited\\)" change)
@@ -34,15 +39,21 @@ or a simple file ,前提是emacs.exe emacs 在$PATH路径下"
   "byte compile all by el files under ~/.emacs.d/site-lisp/ except cedet ."
   (interactive)
   (let ((files  (all-files-under-dir-recursively (expand-file-name "~/.emacs.d/site-lisp/")  "\\.el$" nil
-                                                 "\\.git\\|\\.svn\\|RCS\\|rcs\\|CVS\\|cvs\\|joseph_init.el$\\|malabar-1.5-SNAPSHOT\\b\\|\\bicicles\\b" t
-                                                        )))
+                                                 "\\.git\\|\\.svn\\|RCS\\|rcs\\|CVS\\|cvs\\|joseph_init.el$\\|malabar-1.5-SNAPSHOT\\b\\|\\bicicles\\b\\|joseph_init.el$" t
+                                                 ))
+        (i 0)
+        300eles)
     ;; (setq files  (joseph-delete-matched-files files "/cedet-1.0/" t ));;不对cedet 进行编译
     ;;这两句话保证joseph_init.el最后编译,如果先编译了它,那么所有的el都会被load进来,
     ;;包括folding.el ,不知道什么原因byte-compile-file 与folding好像有冲突
     ;;如果一个el里fold了,那么隐藏的内容无法被编译
     ;; (setq files (joseph-delete-matched-files files "joseph_init.el$"))
-     (joseph-byte-compile-files-outside files)
-    (joseph-byte-compile-files-outside  (expand-file-name "~/.emacs.d/site-lisp/joseph/joseph_init.el"))
-    ))
-
-
+    (while (< i (length files))
+      (add-to-list '300eles (nth i files))
+      (when (and  (= 0 (% i 300)) (> i 0 ))
+        (joseph-byte-compile-files-outside 300eles)
+        (setq 300eles nil)
+        )
+      (setq i (1+ i)))
+    (add-to-list '300eles   (expand-file-name "~/.emacs.d/site-lisp/joseph/joseph_init.el") t)
+    (joseph-byte-compile-files-outside 300eles)))
