0592646b6176169bd587accd03a8412c34abac85
jixiuf <jixiuf@gmail.com>
2011-10-31 16:32:12 +0800

    rename func

--- site-lisp/joseph/joseph_byte_compile_include.el
+++ site-lisp/joseph/joseph_byte_compile_include.el
@@ -1,9 +1,9 @@
  ;; -*-no-byte-compile: t; coding:utf-8  -*-
 (add-to-list 'load-path "~/.emacs.d/site-lisp/joseph-file-util/")
 (require 'joseph-file-util)
-(defvar user-load-path (joseph-all-subdirs-under-dir-recursively
-                        (expand-file-name "~/.emacs.d/site-lisp/")
-                        "\\.git\\|\\.svn\\|RCS\\|rcs\\|CVS\\|cvs\\|doc\\|syntax\\|templates\\|tests\\|icons\\|lib\\|testing\\|etc\\|script"))
+(defvar user-load-path (all-subdir-under-dir-recursively
+                        (expand-file-name "~/.emacs.d/site-lisp/") nil nil
+                        "\\.git\\|\\.svn\\|RCS\\|rcs\\|CVS\\|cvs\\|doc\\|syntax\\|templates\\|tests\\|icons\\|lib\\|testing\\|etc\\|script" t))
 (dolist (path user-load-path) (add-to-list 'load-path path))
 
 (provide 'joseph_byte_compile_include)
