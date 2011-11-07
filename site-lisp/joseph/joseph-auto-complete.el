96b8f4979978e281e9a94f5c8123d00375f7c8ce
jixiuf <jixiuf@gmail.com>
2011-11-07 20:50:51 +0800

    (add-to-list 'ac-modes 'erlang-shell-mode)

--- site-lisp/joseph/joseph-auto-complete.el
+++ site-lisp/joseph/joseph-auto-complete.el
@@ -97,6 +97,7 @@
 (add-to-list 'ac-modes 'nxml-mode)
 (add-to-list 'ac-modes 'nxhtml-mode)
 (add-to-list 'ac-modes 'erlang-mode)
+(add-to-list 'ac-modes 'erlang-shell-mode)
 
 ;;(setq ac-ignore-case 'smart);; 智能的处理大小写的匹配 ，当有大写字母的时候不忽略大小写，
 (setq ac-ignore-case nil)
