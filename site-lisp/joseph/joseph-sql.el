141070a0364636e2300eb0fed631e280a13753c1
jixiuf <jixiuf@gmail.com>
2011-11-12 16:40:47 +0800

    move create table to joseph sqlserver.el

--- site-lisp/joseph/joseph-sql.el
+++ site-lisp/joseph/joseph-sql.el
@@ -21,14 +21,13 @@
 
 ;;; Commentary:
 
+
 ;;
 
 ;;; Commands:
 ;;
 ;; Below are complete command list:
 ;;
-;;  `sqlserver-create-table'
-;;    做项目的时候用到的自动将excel表格格式的，创建成建表语句。region的格式如上面注释，注意顶格写
 ;;
 ;;; Customizable Options:
 ;;
@@ -42,6 +41,12 @@
 (setq sql-input-ring-file-name "~/.emacs.d/cache/sql-cmd-hist")
 ;;(setq comint-input-ring-size 500)
 
+;;在普通的sql mode 中
+;; `C-cC-b' send buffer content to *SQL* buffer中去执行。
+;; `C-cC-r' send 选中区域到 *SQL* buffer中去执行。
+;; `C-cC-c' (sql-send-paragraph)
+;;  以上命令的前提是当前buffer与*SQL* 进行了关联
+;;下面这个hook，如果在启用*SQL* 时已经有sql-mode 的buffer了，则将其与*SQL* 进行关联
 (add-hook 'sql-interactive-mode-hook 'my-sql-set-buffer)
 (defun my-sql-set-buffer ()
   "Sets the SQLi buffer for all unconnected SQL buffers.
@@ -64,104 +69,5 @@ Called from `sql-interactive-mode-hook'."
 ;; (autoload 'sqlserver-mode "joseph-sqlserver" " mode for editing sqlserver script(fn &optional ARG)" t nil)
 
 
-;;;; sqlserver-create-table()
-;;  sqlserver-create-table 会根据格式如下的一段内容，自动生成sql语句，创建这样一张表
-;; STOCK_ID									IDENTITY
-;; SEMIFINISHER_ID									INT
-;; STOCK_WEIGHT									DECIMAL					18,2
-;; STOCK_YEAR_MONTH									DATATIME
-;; START_WEIGHT									DECIMAL					18,2
-;; CREATE_DATETIME									DATETIME
-;; CREATER_ID									NVARCHAR					20
-;; UPDATE_DATETIME									DATETIME
-;; UPDATER_ID									NVARCHAR					20
-;; DELETE_FLG									NVARCHAR					1
-;;;###autoload
-(defun sqlserver-create-table (region-begin region-end)
-  "做项目的时候用到的自动将excel表格格式的，创建成建表语句。region的格式如上面注释，注意顶格写"
-  (interactive "r")
-  (let ( (tablename (read-string "tablename:"))
-        (region-string (buffer-substring-no-properties region-begin region-end))
-        (case-fold-search t)
-        column-scripts script)
-    (with-temp-buffer
-      (insert region-string)
-      (insert "\n  ")
-      (replace-string "datatime" "datetime" t (point-min) (point-max))
-      (goto-char (point-min))
-      (while (<  (line-number-at-pos )(count-lines (point-min)(point-max) ))
-        (beginning-of-line)
-        (forward-sexp 2)
-        (when (thing-at-point 'word)
-          (if (string-match "nvarchar"  (thing-at-point 'word))
-              (progn
-                (forward-sexp)
-                (backward-sexp)
-                (insert "(")
-                (forward-sexp)
-                (insert ") not null,")
-                )
-            (if (string-match "int"  (thing-at-point 'word))
-                (progn
-                  (insert " not null,")
-                  )
-              (if (string-match "datetime"  (thing-at-point 'word))
-                  (progn
-                    (insert " not null,")
-                    )
-                (if (string-match "DECIMAL"  (thing-at-point 'word))
-                    (progn
-                      (forward-sexp)
-                      (backward-sexp)
-                      (insert "(")
-                      (forward-sexp 2)
-                      (insert ") not null,")
-                      )
-                  (if (string-match "identity"  (thing-at-point 'word))
-                      (progn
-                        (backward-sexp )
-                        (insert " int ")
-                        (forward-sexp)
-                        (insert "(1, 1) NOT NULL,")
-                        )
-                    )
-                  )
-
-                )
-              )
-            )
-          )
-        (forward-line)(end-of-line)
-        )
-      (setq column-scripts (buffer-substring-no-properties (point-min) (point-max)))
-      )
-    (with-temp-buffer
-      (insert (format "USE [HAIHUA_SMART];
-       GO
-       SET ANSI_NULLS ON;
-       GO
-       SET QUOTED_IDENTIFIER ON;
-       GO
-       CREATE TABLE [dbo].[%s] ( \n" tablename))
-
-      (insert column-scripts "\n")
-      (insert " )
-       ON [PRIMARY];
-       GO ")
-      (setq script (buffer-substring-no-properties (point-min) (point-max)))
-      )
-    (let ((buf (make-temp-name "*sql-temp-sqlserver-create-table")))
-      (switch-to-buffer buf)
-      (with-current-buffer buf
-        (sql-mode)
-        (insert script)
-        (indent-region (point-min) (point-max))
-        )
-      )
-
-
-    )
-  )
-;;osql -U haihua -P hh  -S 172.20.68.10 -d HAIHUA_SMART -q "select * from sysobjects"
 (provide 'joseph-sql)
 ;;; joseph-sql.el ends here
