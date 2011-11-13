47fd72c784efbebd8920f26b4734e7fefa9e2852
jixiuf <jixiuf@gmail.com>
2011-11-13 12:37:41 +0800

    ("\\.wav" "aplay" (file))

--- site-lisp/joseph/joseph-openwith-linux.el
+++ site-lisp/joseph/joseph-openwith-linux.el
@@ -28,9 +28,10 @@
   (interactive)
   (let ((file-name (if (equal major-mode 'dired-mode )  (dired-get-filename) (buffer-file-name)))
         (openwith-associations
-         '(("\\.pdf$" "acroread" (file)) ("\\.mp3$" "mplayer" (file) )
+         '(("\\.pdf$" "acroread" (file)) ("\\.mp3$" "mpg123" (file) )
            ("\\.RM$\\|\\.RMVB$\\|\\.avi$\\|\\.AVI$\\|\\.flv$\\|\\.mp4\\|\\.mkv$\\|\\.rmvb$" "mplayer" (file) )
            ("\\.jpe?g$\\|\\.png$\\|\\.bmp\\|\\.gif$" "gpicview" (file))
+           ("\\.wav" "aplay" (file))
            ("\\.CHM$\\|\\.chm$" "chmsee"  (file) ))))
     (if (string-match "\\.html?$" file-name)
         (if (> (string-to-number (shell-command-to-string "pgrep firefox | wc -l")) 0)
