b1874ef10f9b405ea2d86c87a2f2d0a8aec42e82
jixiuf <jixiuf@gmail.com>
2011-11-14 18:03:05 +0800

    update

--- install-w32.bat
+++ install-w32.bat
@@ -1,4 +1,10 @@
-set emacs_dir=D:\usr\emacs-24.0.91
+set emacs_dir=D:\usr\emacs-24.0.90
 set HOME=d:\
 cd bin\mew-6.4
 mew.exe
+
+rem »Øµ½~/.emacs.d
+cd ..\.. 
+xcopy /r /e /h /y  dll\* %emacs_dir%\bin
+xcopy /r /e /h   script\msys-bashrc %HOME%\.bashrc
+pause
