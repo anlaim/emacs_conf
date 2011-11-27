5b7c2eaecf6a8d8e2da8f6ef58a36e42fc7cea35
jixiuf <jixiuf@gmail.com>
2011-11-28 00:07:52 +0800

    modified:   joseph-vc.el

--- site-lisp/joseph/joseph-vc.el
+++ site-lisp/joseph/joseph-vc.el
@@ -33,6 +33,7 @@
 (define-key-lazy vc-dir-mode-map ":" 'vc-command "vc-dir")
 (define-key-lazy vc-dir-mode-map "u" 'vc-up-dir "vc-dir") ;到上层目录
 (define-key-lazy vc-dir-mode-map  [?\H-m] 'vc-dir-unmark "vc-dir");;原来u的命令，现在C-m
+(define-key-lazy vc-dir-mode-map "c" 'vc-dir "vc-dir");;
 
 
 ;;在使用diff比较两个文件时，调用此函数，会
