4f20e26f1e9921ba76da7560115913def6451c83
jixiuf <jixiuf@gmail.com>
2011-11-27 16:43:19 +0800

    (require 'lusty-explorer)

--- site-lisp/joseph/joseph-anything.el
+++ site-lisp/joseph/joseph-anything.el
@@ -56,6 +56,17 @@
 ;;      )
 ;;   )
 
+(defun my-lusty-hook ()
+  "lusty是`find-file'的替代品，通过`C-f' `C-b' `C-n' `C-p' 进行选
+择，及有ido,anything iswitchb的过滤功能."
+  (define-key lusty-mode-map "\C-e" 'lusty-select-match);;work like TAB
+  (define-key lusty-mode-map " " 'lusty-select-match);;work like TAB
+
+  (define-key lusty-mode-map "\C-j" 'lusty-open-this)
+  (define-key lusty-mode-map "\C-d" 'lusty-launch-dired);;call dired
+  (define-key lusty-mode-map "\r" 'lusty-select-current-name);;RET create new file
+  (define-key lusty-mode-map  [?\H-m] 'lusty-highlight-previous-column );;C-m work like C-b
+  )
 
 (eval-after-load 'anything-config
   '(progn
@@ -75,12 +86,12 @@
                  "*Ibuffer*"
                  )))
      (setq anything-c-locate-command
-       (case system-type
-         ('gnu/linux "locate -i -r %s")
-         ('berkeley-unix "locate -i %s")
-         ('windows-nt "es -r %s")      ;remove -i case senetitave 忽略 大小写
-         (t "locate %s"))
-       )
+           (case system-type
+             ('gnu/linux "locate -i -r %s")
+             ('berkeley-unix "locate -i %s")
+             ('windows-nt "es -r %s")      ;remove -i case senetitave 忽略 大小写
+             (t "locate %s"))
+           )
 
      (set-keymap-parent ctl-w-map anything-command-map)
      (setq anything-for-files-prefered-list
@@ -131,6 +142,10 @@
      (define-key anything-c-read-file-map (kbd "C-,") 'minibuffer-up-parent-dir)
 
      (anything-completion-mode)
+     (add-to-list 'anything-completing-read-handlers-alist '(lusty-file-explorer . nil));;对于lusty 不使用anything engine
+     (require 'lusty-explorer)
+     (global-set-key [remap find-file] 'lusty-file-explorer) ;C-xC-f
+     (add-hook 'lusty-setup-hook 'my-lusty-hook)
 
      (defun anything-man-woman (&optional arg)
        "Preconfigured `anything' for Man and Woman pages."
@@ -139,6 +154,16 @@
 
      ))
 
+;; (defun lusty-delete-char-or-dired(&optional N KILLFLAG)
+;;   "if current position in minibuffer is end of line ,then
+;; call `lusty-launch-dired' else  `delete-char'"
+;;   (interactive "p\nP")
+;;   (if (eolp)
+;;       (call-interactively 'lusty-launch-dired)
+;;     (delete-char (or 1 N) KILLFLAG)
+
+;;     )
+;;   )
 ;; (require 'anything-startup)
 (require 'anything-config)
 (require 'joseph-anything-filelist)
