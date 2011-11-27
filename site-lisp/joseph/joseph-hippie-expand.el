894f902a6c3552aec6793631b3c2bccbdebb7338
jixiuf <jixiuf@gmail.com>
2011-11-27 13:07:29 +0800

    yas/hippie-try-expand

--- site-lisp/joseph/joseph-hippie-expand.el
+++ site-lisp/joseph/joseph-hippie-expand.el
@@ -7,22 +7,22 @@
 ;(autoload 'senator-try-expand-semantic "senator")
 (autoload 'try-joseph-dabbrev-substring "joseph-command" "让hippie-expand支持子串匹配" nil)
 (setq-default hippie-expand-try-functions-list
-     '(
-;        senator-try-expand-semantic
-        try-expand-dabbrev
-        try-joseph-dabbrev-substring
-        try-expand-dabbrev-visible
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-expand-list
-        try-expand-list-all-buffers
-        try-expand-line
-        try-expand-line-all-buffers
-        try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-whole-kill
-        )
-)
+              '(
+                yas/hippie-try-expand
+                try-expand-dabbrev
+                try-joseph-dabbrev-substring
+                senator-try-expand-semantic
+                try-expand-dabbrev-visible
+                try-expand-dabbrev-all-buffers
+                try-expand-dabbrev-from-kill
+                try-expand-list
+                try-expand-list-all-buffers
+                try-expand-line
+                try-expand-line-all-buffers
+                try-complete-file-name-partially
+                try-complete-file-name
+                try-expand-whole-kill
+                )
+              )
 
 (provide 'joseph-hippie-expand)
-
