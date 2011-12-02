a3acb3029d1d5e9d10d2a46fe926add2074d4e7c
jixiuf <jixiuf@gmail.com>
2011-12-02 22:53:45 +0800

    sprintf(cmd,"escript %s %s" ,argv[1],argv[2]); /* on linux */

--- bin/eflymake.c
+++ bin/eflymake.c
@@ -11,15 +11,20 @@
   you need add D:\usr\erl5.8.5\bin to %Path%
   flymake for erlang
   with eflymake.erl
+  accept 2 params
+  argv[1] is the full path of eflymake.erl 是eflymake.erl 文件的绝对路径
+  argv[2] is the erlang source file you want to check
+  for example:
+  eflymake /home/jixiuf/.emacs.d/bin/eflymake.erl /tmp/tmp.erl
  */
 int main(int argc, char *argv[]){
   char cmd[1024] ;
-  if (argc==0){
+  if (argc<2){
     printf("need a arg as erlang src file name.");
     return 1;
   }
-  /* sprintf(cmd,"escript.exe %s" ,argv[1]); /\* on windows *\/ */
-  sprintf(cmd,"escript %s" ,argv[1]); /* on linux */
+  /* sprintf(cmd,"escript.exe %s %s" ,argv[1],argv[2]); /\* on windows *\/ */
+  sprintf(cmd,"escript %s %s" ,argv[1],argv[2]); /* on linux */
   system(cmd);
   return 0;
 }
