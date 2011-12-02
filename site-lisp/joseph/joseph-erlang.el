3804cd51b8fd2f4493e2be7b5d81992ae00f5ec6
jixiuf <jixiuf@gmail.com>
2011-12-02 15:09:31 +0800

    custome erlang mode

--- site-lisp/joseph/joseph-erlang.el
+++ site-lisp/joseph/joseph-erlang.el
@@ -2,7 +2,7 @@
 
 ;; Description: erlang mode config
 ;; Created: 2011-11-07 10:35
-;; Last Updated: Joseph 2011-11-27 12:01:11 星期日
+;; Last Updated: Joseph 2011-12-02 14:22:47 星期五
 ;; Author: 纪秀峰  jixiuf@gmail.com
 ;; Maintainer:  纪秀峰  jixiuf@gmail.com
 ;; Keywords: erlang
@@ -43,8 +43,37 @@
   (setq exec-path (cons "d:/usr/erl5.8.5/bin" exec-path))
   (setenv "PATH" (concat (getenv "PATH") ";" (get-system-file-path  "d:/usr/erl5.8.5/bin")))
   )
+;;; my-erlang-mode-hook
+;; add Erlang functions to an imenu menu
+;; (imenu-add-to-menubar "Imenu"); 在菜单栏上添加 Imenu ,我不用它，用anything-imenu 代替。 C-wi
+;; M-h mark子句, C-M-h mark-function
+;; C-cC-q indent-function .不过可用C-M-h 与C-M-\ 结合来完成相同的操作
+;; (erlang-generate-new-clause) C-cC-j  生成一个clause 类似于输入分号 ;
+;; (erlang-clone-arguments)C-cC-y clone 前一个子句的参数到当前子句,此时光标要位于当前子句的参数位置
+;; M-q 可用于格式化注释
+;; C-cC-k erlang-compile ,C-zs compile-dwim 我做了集成。
+;; C-cC-l compile有错，时，显示错误
+;; C-cC-z switch to erlang-shell buffer
+;;;; (erlang-align-arrows ) C-cC-a
+;; sum(L) -> sum(L, 0).
+;; sum([H|T], Sum) -> sum(T, Sum + H);
+;; sum([], Sum) -> Sum.
 
+;; becomes:
 
+;; sum(L)          -> sum(L, 0).
+;; sum([H|T], Sum) -> sum(T, Sum + H);
+;; sum([], Sum)    -> Sum.
+;;;; other
+(defun my-erlang-mode-hook ()
+  (setq inferior-erlang-machine-options '("-sname" "emacs")) ;; erl -sname emacs
+  (local-set-key [remap mark-paragraph] 'erlang-mark-clause) ;M-h mark子句 C-M-h mark-function
+  (local-set-key [remap forward-sentence] 'erlang-end-of-clause) ;M-e 子句尾 (C-M-e function尾)
+  (local-set-key [remap backward-sentence] 'erlang-beginning-of-clause) ;子句首M-a , (C-M-a function首)
+  )
+(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
+
+;;; other
 ;; (when  (equal system-type 'gnu/linux)
 ;;   (setq erlang-root-dir "/usr/local/otp")
 ;;   (setq exec-path (cons "/usr/local/otp/bin" exec-path)))
