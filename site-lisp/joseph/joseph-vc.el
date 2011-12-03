0102fa2e0881b2d48a239718b675268700deb310
jixiuf <jixiuf@gmail.com>
2011-12-03 14:32:44 +0800

    (define-key-lazy vc-dir-mode-map "^" 'vc-up-dir "vc-dir") ;到上层目录

--- site-lisp/joseph/joseph-vc.el
+++ site-lisp/joseph/joseph-vc.el
@@ -31,6 +31,7 @@
 (define-key-lazy vc-dir-mode-map (kbd "F") 'vc-pull "vc-dir") ;;fetch ,git pull ,
 (define-key-lazy vc-dir-mode-map (kbd "d") 'vc-diff "vc-dir");and =
 (define-key-lazy vc-dir-mode-map ":" 'vc-command "vc-dir")
+(define-key-lazy vc-dir-mode-map "^" 'vc-up-dir "vc-dir") ;到上层目录
 (define-key-lazy vc-dir-mode-map "u" 'vc-up-dir "vc-dir") ;到上层目录
 (define-key-lazy vc-dir-mode-map  [?\H-m] 'vc-dir-unmark "vc-dir");;原来u的命令，现在C-m
 (define-key-lazy vc-dir-mode-map "c" 'vc-dir "vc-dir");; change to another vc dir
