d528f2763f22cc4c3db08a37d227777eb46cebca
jixiuf <jixiuf@gmail.com>
2011-12-05 16:46:16 +0800

    (define-key anything-map (kbd "C-,") 'minibuffer-up-parent-dir)

--- site-lisp/joseph/joseph-anything.el
+++ site-lisp/joseph/joseph-anything.el
@@ -104,6 +104,7 @@
 
      (define-key anything-find-files-map (kbd "C-,") 'minibuffer-up-parent-dir)
      (define-key anything-c-read-file-map (kbd "C-,") 'minibuffer-up-parent-dir)
+     (define-key anything-map (kbd "C-,") 'minibuffer-up-parent-dir)
 
      (define-key anything-map (kbd "M-y") 'anything-yank-text-at-point)
      (define-key anything-map (kbd "C-w") nil)
