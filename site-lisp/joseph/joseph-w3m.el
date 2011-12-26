1c675d1c6c912c365200d9d606b2f98df333c9d4
jixiuf <jixiuf@gmail.com>
2011-12-26 14:25:44 +0800

    (require 'w3m-load)

---
+++ site-lisp/joseph/joseph-w3m.el
@@ -0,0 +1,45 @@
+;;; joseph-w3m.el --- w3m config   -*- coding:utf-8 -*-
+
+;; Description: w3m config
+;; Created: 2011-12-26 14:23
+;; Last Updated: Joseph 2011-12-26 14:25:35 星期一
+;; Author: 纪秀峰  jixiuf@gmail.com
+;; Maintainer:  纪秀峰  jixiuf@gmail.com
+;; Keywords: w3m web
+;; URL: http://www.emacswiki.org/emacs/joseph-w3m.el
+
+;; Copyright (C) 2011, 纪秀峰, all rights reserved.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Commands:
+;;
+;; Below are complete command list:
+;;
+;;
+;;; Customizable Options:
+;;
+;; Below are customizable option list:
+;;
+
+;;; Code:
+(require 'w3m-load)
+;; M-x : w3m
+(setq-default w3m-command "w3m")
+(provide 'joseph-w3m)
+;;; joseph-w3m.el ends here
