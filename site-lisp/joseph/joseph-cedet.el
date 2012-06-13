64b7d3887411b28f2db5900411f50eff8552ceea
jixiuf <jixiuf@gmail.com>
2012-06-14 00:38:17 +0800

    cedet

--- site-lisp/joseph/joseph-cedet.el
+++ site-lisp/joseph/joseph-cedet.el
@@ -52,17 +52,17 @@
   )
 (add-hook 'c-mode-common-hook 'alexott/c-mode-cedet-hook)
 
-;; hooks, specific for semantic
-(defun alexott/semantic-hook ()
-;; (semantic-tag-folding-mode 1)
-  (imenu-add-to-menubar "TAGS")
- )
-(add-hook 'semantic-init-hooks 'alexott/semantic-hook)
+;; ;; hooks, specific for semantic
+;; (defun alexott/semantic-hook ()
+;; ;; (semantic-tag-folding-mode 1)
+;;   (imenu-add-to-menubar "TAGS")
+;;  )
+;; (add-hook 'semantic-init-hooks 'alexott/semantic-hook)
 
-(custom-set-variables
- '(semantic-idle-scheduler-idle-time 3)
- '(semantic-self-insert-show-completion-function (lambda nil (semantic-ia-complete-symbol-menu (point))))
- '(global-semantic-tag-folding-mode t nil (semantic-util-modes)))
+;; (custom-set-variables
+;;  ;; '(semantic-idle-scheduler-idle-time 3)
+;;  '(semantic-self-insert-show-completion-function (lambda nil (semantic-ia-complete-symbol-menu (point))))
+;;  '(global-semantic-tag-folding-mode t nil (semantic-util-modes)))
 ;;(global-semantic-folding-mode 1)
 
 ;; gnu global support
@@ -77,28 +77,28 @@
 ;; (semantic-add-system-include "~/exp/include" 'c++-mode)
 ;; (semantic-add-system-include "~/exp/include" 'c-mode)
 
-(defun recur-list-files (dir re)
-  "Returns list of files in directory matching to given regex"
-  (when (file-accessible-directory-p dir)
-    (let ((files (directory-files dir t))
-          matched)
-      (dolist (file files matched)
-        (let ((fname (file-name-nondirectory file)))
-          (cond
-           ((or (string= fname ".")
-                (string= fname "..")) nil)
-           ((and (file-regular-p file)
-                 (string-match re fname))
-            (setq matched (cons file matched)))
-           ((file-directory-p file)
-            (let ((tfiles (recur-list-files file re)))
-              (when tfiles (setq matched (append matched tfiles)))))))))))
+;; (defun recur-list-files (dir re)
+;;   "Returns list of files in directory matching to given regex"
+;;   (when (file-accessible-directory-p dir)
+;;     (let ((files (directory-files dir t))
+;;           matched)
+;;       (dolist (file files matched)
+;;         (let ((fname (file-name-nondirectory file)))
+;;           (cond
+;;            ((or (string= fname ".")
+;;                 (string= fname "..")) nil)
+;;            ((and (file-regular-p file)
+;;                  (string-match re fname))
+;;             (setq matched (cons file matched)))
+;;            ((file-directory-p file)
+;;             (let ((tfiles (recur-list-files file re)))
+;;               (when tfiles (setq matched (append matched tfiles)))))))))))
 
-(defun c++-setup-boost (boost-root)
-  (when (file-accessible-directory-p boost-root)
-    (let ((cfiles (recur-list-files boost-root "\\(config\\|user\\)\\.hpp")))
-      (dolist (file cfiles)
-        (add-to-list 'semantic-lex-c-preprocessor-symbol-file file)))))
+;; (defun c++-setup-boost (boost-root)
+;;   (when (file-accessible-directory-p boost-root)
+;;     (let ((cfiles (recur-list-files boost-root "\\(config\\|user\\)\\.hpp")))
+;;       (dolist (file cfiles)
+;;         (add-to-list 'semantic-lex-c-preprocessor-symbol-file file)))))
 
 
 ;;
@@ -145,22 +145,18 @@
 ;; ))
 
 ;; my functions for EDE
-(defun alexott/ede-get-local-var (fname var)
-  "fetch given variable var from :local-variables of project of file fname"
-  (let* ((current-dir (file-name-directory fname))
-         (prj (ede-current-project current-dir)))
-    (when prj
-      (let* ((ov (oref prj local-variables))
-            (lst (assoc var ov)))
-        (when lst
-          (cdr lst))))))
+;; (defun alexott/ede-get-local-var (fname var)
+;;   "fetch given variable var from :local-variables of project of file fname"
+;;   (let* ((current-dir (file-name-directory fname))
+;;          (prj (ede-current-project current-dir)))
+;;     (when prj
+;;       (let* ((ov (oref prj local-variables))
+;;             (lst (assoc var ov)))
+;;         (when lst
+;;           (cdr lst))))))
 
 ;; setup compile package
 ;; TODO: allow to specify function as compile-command
-(require 'compile)
-(setq compilation-disable-input nil)
-(setq compilation-scroll-output t)
-(setq mode-compile-always-save-buffer-p t)
 
 ;; (defun alexott/compile ()
 ;;   "Saves all unsaved buffers, and runs 'compile'."
@@ -177,27 +173,27 @@
 ;; (global-set-key [f9] 'alexott/compile)
 
 ;;
-(defun alexott/gen-std-compile-string ()
-  "Generates compile string for compiling CMake project in debug mode"
-  (let* ((current-dir (file-name-directory
-                       (or (buffer-file-name (current-buffer)) default-directory)))
-         (prj (ede-current-project current-dir))
-         (root-dir (ede-project-root-directory prj))
-         )
-    (concat "cd " root-dir "; make -j2")))
+;; (defun alexott/gen-std-compile-string ()
+;;   "Generates compile string for compiling CMake project in debug mode"
+;;   (let* ((current-dir (file-name-directory
+;;                        (or (buffer-file-name (current-buffer)) default-directory)))
+;;          (prj (ede-current-project current-dir))
+;;          (root-dir (ede-project-root-directory prj))
+;;          )
+;;     (concat "cd " root-dir "; make -j2")))
 
 ;;
-(defun alexott/gen-cmake-debug-compile-string ()
-  "Generates compile string for compiling CMake project in debug mode"
-  (let* ((current-dir (file-name-directory
-                       (or (buffer-file-name (current-buffer)) default-directory)))
-         (prj (ede-current-project current-dir))
-         (root-dir (ede-project-root-directory prj))
-         (subdir "")
-         )
-    (when (string-match root-dir current-dir)
-      (setf subdir (substring current-dir (match-end 0))))
-    (concat "cd " root-dir "Debug/" "; make -j3")))
+;; (defun alexott/gen-cmake-debug-compile-string ()
+;;   "Generates compile string for compiling CMake project in debug mode"
+;;   (let* ((current-dir (file-name-directory
+;;                        (or (buffer-file-name (current-buffer)) default-directory)))
+;;          (prj (ede-current-project current-dir))
+;;          (root-dir (ede-project-root-directory prj))
+;;          (subdir "")
+;;          )
+;;     (when (string-match root-dir current-dir)
+;;       (setf subdir (substring current-dir (match-end 0))))
+;;     (concat "cd " root-dir "Debug/" "; make -j3")))
 
 ;;; emacs-rc-cedet.el ends here
 
