251313d970f97b42ed4a40d05b5c76b3701b6673
jixiuf <jixiuf@gmail.com>
2012-11-25 16:37:05 +0800

    [纪秀峰]:iedit 受影响的文件:     site-lisp/lisp/joseph-loaddefs.el     site-lisp/joseph/joseph-iedit.el

--- site-lisp/joseph/joseph-iedit.el
+++ site-lisp/joseph/joseph-iedit.el
@@ -1,7 +1,9 @@
-(autoload 'iedit-mode "iedit" "Edit current search matches")
-(global-set-key (kbd "C-M-;") 'iedit-mode)
-(define-key isearch-mode-map (kbd "C-M-;") 'iedit-mode)
-(eval-after-load "iedit"
-  '(define-key iedit-mode-map (kbd "C-g") 'iedit-mode))
+;; (autoload 'iedit-mode "iedit" "Edit current search matches")
+(setq-default iedit-toggle-key-default (kbd "C-M-;"))
+(autoload 'iedit-mode-from-isearch "iedit" "enable iedit-mode when in isearch mode")
+
+(define-key global-map iedit-toggle-key-default 'iedit-mode)
+(define-key isearch-mode-map iedit-toggle-key-default 'iedit-mode-from-isearch)
+
 
 (provide 'joseph-iedit)
