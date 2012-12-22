1f18443d29d95b26e76ae65839a1f2fc938e521c
jixiuf <jixiuf@gmail.com>
2012-12-22 10:26:05 +0800

    [纪秀峰]:update 受影响的文件:     site-lisp/popwin-el     site-lisp/joseph/joseph-ido.el     site-lisp/helm-etags-plus     site-lisp/helm     site-lisp/distel

--- site-lisp/joseph/joseph-ido.el
+++ site-lisp/joseph/joseph-ido.el
@@ -2,7 +2,7 @@
 
 ;; Description: ido
 ;; Created: 2011-11-10 23:23
-;; Last Updated: 纪秀峰 2012-12-13 10:52:27 星期四
+;; Last Updated: 纪秀峰 2012-12-13 13:56:00 星期四
 ;; Author: 纪秀峰  jixiuf@gmail.com
 ;; Maintainer:  纪秀峰  jixiuf@gmail.com
 ;; Keywords: ido
@@ -64,6 +64,7 @@
 (defun ido-my-keys ()
   "Add my keybindings for ido."
   ;; (define-key ido-completion-map (kbd "C-j") 'ido-exit-minibuffer) ;select or expand
+  (define-key ido-completion-map (kbd "C-e") 'ido-exit-minibuffer) ;select or expand
   (define-key ido-completion-map [?\H-m] 'ido-exit-minibuffer) ;select or expand
   (define-key ido-completion-map "\r" 'ido-select-text);new buffer
   (define-key ido-completion-map (kbd "C-,") 'ido-up-directory)
