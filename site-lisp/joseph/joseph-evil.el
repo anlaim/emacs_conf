68b2759c312e6b3185eff65df7d522534bdd06a0
jixiuf <jixiuf@gmail.com>
2013-01-10 12:06:29 +0800

    [纪秀峰]:evil magit binding 受影响的文件:     site-lisp/joseph/joseph-evil.el

--- site-lisp/joseph/joseph-evil.el
+++ site-lisp/joseph/joseph-evil.el
@@ -1,12 +1,27 @@
 ;; https://github.com/mbriggs/.emacs.d/blob/master/my-keymaps.el
+;; http://dnquark.com/blog/2012/02/emacs-evil-ecumenicalism/
+;; https://github.com/cofi/dotfiles/blob/master/emacs.d/config/cofi-evil.el
+;; 当v 选择到行尾时是否包含换行符
+(setq-default evil-want-visual-char-semi-exclusive t)
 (setq-default evil-toggle-key "C-w z")
 (setq-default evil-want-C-i-jump nil)
 (require 'evil)
 (evil-mode 1)
 (setq evil-want-fine-undo t)
+;; C-e ,到行尾时,光标的位置是在最后一个字符后,还是在字符上
+(setq evil-move-cursor-back nil) ;;and maybe also:
+(setq evil-highlight-closing-paren-at-point-states nil)
 
-(setq evil-default-cursor (quote (t "white")))
-(setq evil-emacs-state-cursor '("red" box))
+(setq evil-normal-state-tag (propertize "N" 'face '((:background "green" :foreground "black")))
+      evil-emacs-state-tag (propertize "E" 'face '((:background "orange" :foreground "black")))
+      evil-insert-state-tag (propertize "I" 'face '((:background "red")))
+      evil-motion-state-tag (propertize "M" 'face '((:background "blue")))
+      evil-visual-state-tag (propertize "V" 'face '((:background "grey80" :foreground "black")))
+      evil-operator-state-tag (propertize "O" 'face '((:background "purple"))))
+
+
+(setq evil-default-cursor      '(t "white"))
+(setq evil-emacs-state-cursor  '("red" box))
 (setq evil-normal-state-cursor '("gray" box))
 (setq evil-visual-state-cursor '("gray" box))
 (setq evil-insert-state-cursor '("gray" bar))
@@ -21,14 +36,6 @@
 (evil-declare-motion 'joseph-scroll-half-screen-down)
 (evil-declare-motion 'joseph-scroll-half-screen-up)
 
-;; 默认dird 的r 修改了, 不是 wdired-change-to-wdired-mode,现在改回
-(eval-after-load 'dired
-  '(progn
-     ;; use the standard Dired bindings as a base
-     ;; (evil-make-overriding-map dired-mode-map 'normal t)
-     (evil-add-hjkl-bindings dired-mode-map 'normal
-       "r" 'wdired-change-to-wdired-mode                ;
-       )))
 
 (define-key evil-normal-state-map (kbd "C-w") 'ctl-w-map)
 (define-key evil-insert-state-map (kbd "C-w") 'ctl-w-map)
@@ -52,8 +59,11 @@
 (define-key evil-motion-state-map "n" nil)
 (define-key evil-motion-state-map "N" nil)
 (define-key evil-normal-state-map "\C-r" nil)
+;; 交换y p 的功能
 (define-key evil-normal-state-map "y" 'evil-paste-after)
 (define-key evil-normal-state-map "Y" 'evil-paste-before)
+(define-key evil-normal-state-map "p" 'evil-yank)
+(define-key evil-normal-state-map "P" 'evil-yank-line)
 ;; (define-key evil-normal-state-map (kbd "C-y") 'yank)
 
 ;; esc
@@ -65,7 +75,26 @@
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
+;; 默认dird 的r 修改了, 不是 wdired-change-to-wdired-mode,现在改回
+;; (eval-after-load 'dired
+;;   '(progn
+;;      ;; use the standard dired bindings as a base
+;;      ;; (evil-make-overriding-map dired-mode-map 'normal t)
+;;      ))
+(evil-define-key 'normal dired-mode-map "r" 'wdired-change-to-wdired-mode)
+
 ;; (evil-define-key 'normal magit-log-edit-mode-map "q" 'magit-log-edit-commit)
+(evil-add-hjkl-bindings magit-branch-manager-mode-map 'normal
+  "p" 'magit-goto-previous-section
+  ;; "K" 'magit-discard-item
+  "L" 'magit-key-mode-popup-logging)
+
+;; magit-stats中,j k 上下移动
+(evil-add-hjkl-bindings magit-status-mode-map 'emacs
+  "K" 'magit-discard-item
+  "l" 'magit-key-mode-popup-logging
+  "h" 'magit-toggle-diff-refine-hunk)
+
 (provide 'joseph-evil)
 
 ;; Local Variables:
