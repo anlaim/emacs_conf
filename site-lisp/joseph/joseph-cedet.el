079378adb489316975a5401f1c22558a3e794ff4
jixiuf <jixiuf@gmail.com>
2013-01-16 10:43:58 +0800

    [纪秀峰]:fix byte compile warning
    受影响的文件:
        joseph-cedet.el

--- site-lisp/joseph/joseph-cedet.el
+++ site-lisp/joseph/joseph-cedet.el
@@ -1,4 +1,11 @@
 ;;; -*- coding:utf-8 -*-
+(eval-when-compile
+  (add-to-list 'load-path  (expand-file-name "."))
+  (require 'joseph_byte_compile_include)
+  (require 'senator)
+  (require 'cedet-compat)
+  )
+
 (require 'cedet)
 (semantic-load-enable-excessive-code-helpers)
 ;;(semantic-load-enable-semantic-debugging-helpers)
