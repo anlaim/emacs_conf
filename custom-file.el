a539297ed5be3272db65a8328ab7b1737b047786
jixiuf <jixiuf@gmail.com>
2013-01-22 12:03:48 +0800

    [纪秀峰]:todo keywords
    受影响的文件:
        custom-file.el

--- custom-file.el
+++ custom-file.el
@@ -11,6 +11,7 @@
   (font-lock-add-keywords
    mode
    '(("\\<\\(FIXME\\|TODO\\|Todo\\|HACK\\|todo\\):" 1  'font-lock-todo-face prepend)
+     ("@\\<\\(FIXME\\|TODO\\|Todo\\|HACK\\|todo\\)" 1  'font-lock-todo-face prepend)
      ("\\<\\(DONE\\|Done\\|done\\):" 1 'font-lock-done-face t)
      ("\\<\\(and\\|or\\|not\\)\\>" . font-lock-keyword-face)
      )))
