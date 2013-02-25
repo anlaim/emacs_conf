7933d76e5a79f2ebcf370fb64e3b6860a1ae2c6e
jixiuf <jixiuf@gmail.com>
2013-02-25 14:48:30 +0800

    [纪秀峰]:gitsh 受影响的文件:     bin/gitsh.exe     bin/gitsh.c

---
+++ bin/gitsh.c
@@ -0,0 +1,55 @@
+/* gitsh for msys  */
+/* 主要解决 emacs与git合作时，提交日志中含中文，乱码问题 */
+#include <stdio.h>
+#include <stdlib.h>
+// #include <direct.h>
+#include <string.h>
+// .git/COMMIT_EDITMSG
+int get_commit_editmsg_path(char** Path){
+  char*  tempPath = getenv("TEMP");
+  char* path=malloc(strlen(tempPath)+strlen("/COMMIT_EDITMSG")+1);
+  strncpy(path,tempPath,strlen(tempPath));
+  strcat(path,"/COMMIT_EDITMSG");
+  *Path= path;
+  return 0;
+}
+int main(int argc, char* argv[])
+{
+  int i;
+  char * lpGBKStr = NULL;
+  char * lpUTF8Str = NULL;
+  int nRetLen = 0;
+  char* cmd=(char*)malloc(1024*1024*10)  ;        /* 10k */
+  char* buf=(char*)malloc(1024*1024*10)  ;        /* 10k */
+  char* Path=NULL;
+  sprintf(cmd,"%s","git");
+
+  if(argc>2&& strcmp(argv[1],"commit")==0&&strcmp(argv[2],"-m")==0){
+    get_commit_editmsg_path(&Path);
+
+    FILE *msgF = fopen( Path, "w+");
+    fclose(msgF);
+
+    sprintf(buf,"%s commit --file=\"%s\" ",cmd,Path);
+    memcpy(cmd,buf,strlen(buf));
+
+    for (i = 4; i < argc; i++){ // i = 4 , skip "-m" "msg"
+      sprintf(buf,"%s \"%s\" ",cmd,argv[i]);
+      memcpy(cmd,buf,strlen(buf));
+    }
+  }else{
+    for (i = 1; i < argc; i++){
+      sprintf(buf,"%s \"%s\" ",cmd,argv[i]);
+      memcpy(cmd,buf,strlen(buf));
+    }
+  }
+  system(cmd );
+
+  if(Path!=NULL){
+    free(Path);
+  }
+  free(cmd);
+  free(buf);
+
+  return 0;
+}
