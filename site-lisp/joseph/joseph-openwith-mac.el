ff9368e0dd9e2bc9424602762403b12762413857
jixiuf <jixiuf@gmail.com>
2013-07-25 00:19:18 +0800

    [纪秀峰]:openwith mac firefox 受影响的文件:     site-lisp/joseph/joseph-openwith-mac.el

--- site-lisp/joseph/joseph-openwith-mac.el
+++ site-lisp/joseph/joseph-openwith-mac.el
@@ -17,9 +17,29 @@
           ;;          ("\\.jpe?g$\\|\\.png$\\|\\.bmp\\|\\.gif$" "gpicview" (file))
           ("\\.CHM$\\|\\.chm$" "chmsee"  (file) )
           )))
+
+(defun browse-url-of-buffer-with-firefox ()
+  "Same as `browse-url-of-buffer' but using Firefox.
+You need Firefox's path in the path environment variable within emacs.
+⁖
+ (setenv \"PATH\" (concat \"C:/Program Files (x86)/Mozilla Firefox/\" \";\" (getenv \"PATH\") ) )
+On Mac OS X, you don't need to. This command makes this shell call:
+ 「open -a Firefox.app http://example.com/」"
+  (interactive)
+  (cond
+   ;; ((string-equal system-type "windows-nt") ; Windows
+   ;;  (shell-command (concat "firefox file://"  (if (equal major-mode 'dired-mode )  (dired-get-filename) (buffer-file-name))))
+   ;;  )
+   ((string-equal system-type "gnu/linux")
+    (shell-command (concat "firefox file://" (if (equal major-mode 'dired-mode )  (dired-get-filename) (buffer-file-name))))
+    )
+   ((string-equal system-type "darwin") ; Mac
+    (shell-command (concat "open -a Firefox.app file://" (if (equal major-mode 'dired-mode )  (dired-get-filename) (buffer-file-name))))
+    ) ))
+
 ;; 第二种打开方式
-;; (define-key-lazy  global-map "\C-\M-j" 'open-with-2-on-linux)
-;; (define-key-lazy  dired-mode-map "\C-\M-j" 'open-with-2-on-linux)
+(define-key-lazy  global-map "\C-\M-j" 'browse-url-of-buffer-with-firefox)
+(define-key-lazy  dired-mode-map "\C-\M-j" 'browse-url-of-buffer-with-firefox)
 
 ;;; linux `C-M-RET' 用pcmanfm文件管理器打开当前目录
 ;; (define-key-lazy dired-mode-map (quote [C-M-return]) 'open-directory-with-pcmanfm)
