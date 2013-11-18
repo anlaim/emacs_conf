283531291c8833fb0fca918c723785d13ea55d57
jixiuf <jixiuf@gmail.com>
2013-11-18 21:34:47 +0800

    push-curline-to-bookmark 去掉行首空格

--- site-lisp/joseph/joseph-helm-lazy.el
+++ site-lisp/joseph/joseph-helm-lazy.el
@@ -3,8 +3,10 @@
 ;;;###autoload
 (defun push-curline-to-bookmark()
   (when (buffer-file-name)
-    (bookmark-set  (concat (buffer-substring-no-properties (line-beginning-position) (line-end-position))
-                           " "  (buffer-file-name)))))
+    (bookmark-set  (replace-regexp-in-string
+                    "^[ \t]*" ""
+                    (buffer-substring-no-properties
+                     (line-beginning-position) (line-end-position))))))
 
 
 (provide 'joseph-helm-lazy)
