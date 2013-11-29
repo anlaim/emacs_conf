b83fba7afec0a3a24390ba5fcaaf0a93be433e28
jixiuf <jixiuf@gmail.com>
2013-11-29 23:02:09 +0800

    [纪秀峰]:tag
    受影响的文件:
        site-lisp/joseph/joseph-tag-lazy.el

--- site-lisp/joseph/joseph-tag-lazy.el
+++ site-lisp/joseph/joseph-tag-lazy.el
@@ -2,15 +2,11 @@
 (eval-when-compile
   (add-to-list 'load-path  (expand-file-name "."))
   (require 'joseph_byte_compile_include)
-  (require 'helm-gtags)
-  (require 'find-func)
-  ;; (require 'bookmark)
-  ;; (require 'helm-etags+)
-  ;; (require 'quick-jump)
   )
-
 ;; (require 'bookmark-cycle)
 (require 'bm)
+(require 'find-func)
+(require 'helm-gtags)
 
 ;;;###autoload
 (defun goto-definition (&optional arg)
