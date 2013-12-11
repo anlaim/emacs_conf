5ee458c8ab239cfec1a2d3646520718f89bb4e0f
jixiuf <jixiuf@gmail.com>
2013-12-11 10:35:00 +0800

    [纪秀峰]:comment linenum config
    受影响的文件:
        joseph-linenum-config.el

--- site-lisp/joseph/joseph-linenum-config.el
+++ site-lisp/joseph/joseph-linenum-config.el
@@ -1,14 +1,14 @@
-;;; byte compile
-(eval-when-compile
-  (add-to-list 'load-path  (expand-file-name "."))
-  (require 'joseph_byte_compile_include)
-  )
-;;; config
+;; ;;; byte compile
+;; (eval-when-compile
+;;   (add-to-list 'load-path  (expand-file-name "."))
+;;   (require 'joseph_byte_compile_include)
+;;   )
+;; ;;; config
 
-(setq-default global-display-line-number-mode t)
-(require 'display-line-number)
-(setq display-line-number-format "%d ")
-(global-display-line-number-mode 1)
+;; (setq-default global-display-line-number-mode t)
+;; (require 'display-line-number)
+;; (setq display-line-number-format "%d ")
+;; (global-display-line-number-mode 1)
 
 
 (provide 'joseph-linenum-config)
