8806f91868082229e22bdc29648f7b09bfc4a643
jixiuf <jixiuf@gmail.com>
2013-12-15 23:03:05 +0800

    toggle-eshell-cd frist delete last unexecuted cmd
    受影响的文件:
        joseph-eshell.el

--- site-lisp/joseph/joseph-eshell.el
+++ site-lisp/joseph/joseph-eshell.el
@@ -1,50 +1,6 @@
-;;; joseph-eshell.el --- config for eshell   -*- coding:utf-8 -*-
+(defvar eshll-toggle-commands '(toggle-eshell-cd toggle-eshell  toggle-shell))
 
-;; Description: config for eshell
-;; Created: 2011-11-25 13:19
-;; Last Updated: Joseph 2012-08-15 00:09:09 星期三
-;; Author: 纪秀峰  jixiuf@gmail.com
-;; Maintainer:  纪秀峰  jixiuf@gmail.com
-;; Keywords: eshell emacs
-;; URL: http://www.emacswiki.org/emacs/joseph-eshell.el
-
-;; Copyright (C) 2011, 纪秀峰, all rights reserved.
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;;
-
-;;; Commands:
-;;
-;; Below are complete command list:
-;;
-;;  `toggle-eshell'
-;;    Start `bash' shell.
-;;  `eshell/clear'
-;;    04Dec2001 - sailor, to clear the eshell buffer.
-;;
-;;; Customizable Options:
-;;
-;; Below are customizable option list:
-;;
-
-;;; Code:
-(defvar eshll-toggle-commands '(toggle-e-bash-cd toggle-e-bash toggle-e-zsh toggle-e-zsh-cd toggle-shell))
-
-(defun toggle-eshell (&optional shell-name shell-buffer-name)
+(defun toggle-eshell-internal (&optional shell-name shell-buffer-name)
   "Start `bash' shell."
   (interactive "sshell name:\nsshell buffer name:")
   (let ((binary-process-input t)
@@ -64,7 +20,7 @@
         ;; If `binary-process-output' is set to `nil', this problem goes
         ;; away, which is fine for files of type `.gz'.
         ;; (ediff-shell shell-name)
-        ;; (shell-buffer-name (or shell-buffer-name "*bash*"))
+        ;; (shell-buffer-name (or shell-buffer-name "*eshell*"))
         )
     ;; (when (equal system-type 'windows-nt)
     ;;   (setq comint-output-filter-functions '(comint-strip-ctrl-m))) 不知原因为何windows 上，加了这句后，shell不显颜色
@@ -95,11 +51,7 @@
         ;; (insert (concat "cd " (concat "\""default-directory "\""))) ;;make sure current directory is default-directory
         ;; (eshell-send-input)
         (set-window-configuration old-window-config)
-        (switch-to-buffer-other-window shell-buffer-name)
-        )
-      )
-    )
-  )
+        (switch-to-buffer-other-window shell-buffer-name)))))
 
 
 (defvar shell-buffer-hist nil)
@@ -120,43 +72,45 @@
         (setq buffer-name (concat "*"  buffer-name "*"))) )
     (setq shell-buffer-hist (delete buffer-name shell-buffer-hist))
     (push buffer-name shell-buffer-hist)
-    buffer-name
-    ))
+    buffer-name))
 
 ;;;###autoload
-(defun toggle-e-bash-cd(&optional arg dir)
+(defun toggle-eshell-cd(&optional arg dir)
   (interactive "P")
   (let ((dest-dir-cd (or dir default-directory))
-        (shell-buffer-name (toggle-shell-completing-read-buffer-name arg "*bash*")))
-    (toggle-eshell "bash" shell-buffer-name)
+        (shell-buffer-name (toggle-shell-completing-read-buffer-name arg "*eshell*")))
+    (toggle-eshell-internal "bash" shell-buffer-name)
     (with-current-buffer shell-buffer-name
       (goto-char (point-max))
+      ;; (eshell-next-prompt 1)            ;
+      (eshell-bol)
+      (delete-region (point) (line-end-position))
       (cd dest-dir-cd)
       (insert (concat "cd \"" dest-dir-cd "\""))
       (eshell-send-input))))
 
 ;;;###autoload
-(defun toggle-e-bash(&optional arg dir)
+(defun toggle-eshell(&optional arg dir)
   (interactive "P")
-  (toggle-eshell "bash"  (toggle-shell-completing-read-buffer-name arg "*bash*")))
+  (toggle-eshell-internal "bash"  (toggle-shell-completing-read-buffer-name arg "*eshell*")))
 
-;;;###autoload
-(defun toggle-e-zsh-cd(&optional arg dir)
-  (interactive "P")
-  (let ((dest-dir-cd (or dir default-directory))
-        (shell-buffer-name (toggle-shell-completing-read-buffer-name arg "*zsh*")))
-    (toggle-eshell "zsh" shell-buffer-name)
-    (with-current-buffer shell-buffer-name
-      (goto-char (point-max))
-      (cd dest-dir-cd)
-      (insert (concat "cd \"" dest-dir-cd "\""))
-      (eshell-send-input)))
-  )
+;; ;;;###autoload
+;; (defun (&optional arg dir)
+;;   (interactive "P")
+;;   (let ((dest-dir-cd (or dir default-directory))
+;;         (shell-buffer-name (toggle-shell-completing-read-buffer-name arg "*zsh*")))
+;;     (toggle-eshell-internal "zsh" shell-buffer-name)
+;;     (with-current-buffer shell-buffer-name
+;;       (goto-char (point-max))
+;;       (cd dest-dir-cd)
+;;       (insert (concat "cd \"" dest-dir-cd "\""))
+;;       (eshell-send-input)))
+;;   )
 
-;;;###autoload
-(defun toggle-e-zsh(&optional arg dir)
-  (interactive "P")
-  (toggle-eshell "zsh"  (toggle-shell-completing-read-buffer-name arg "*zsh*")))
+;; ;;;###autoload
+;; (defun toggle-e-zsh(&optional arg dir)
+;;   (interactive "P")
+;;   (toggle-eshell-internal "zsh"  (toggle-shell-completing-read-buffer-name arg "*zsh*")))
 
 
 ;;using helm.el as the complete engine
