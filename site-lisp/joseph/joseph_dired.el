d274fe8beab16aa1e3f76cab5b10b41a218da3fb
jixiuf <jixiuf@gmail.com>
2013-12-17 13:45:40 +0800

    (define-key-lazy wdired-mode-map (kbd "C-g") 'wdired-abort-changes 'wdired)
    受影响的文件:
        joseph_dired.el

--- site-lisp/joseph/joseph_dired.el
+++ site-lisp/joseph/joseph_dired.el
@@ -135,6 +135,7 @@
 (define-key dired-mode-map (kbd "r") 'wdired-change-to-wdired-mode)
 (setq-default wdired-allow-to-change-permissions t);; writable 时,不仅可以改文件名,还可以改权限
 (define-key-lazy wdired-mode-map (kbd "C-a") 'dired-smart-beginning-of-line 'wdired)
+(define-key-lazy wdired-mode-map (kbd "C-g") 'wdired-abort-changes 'wdired)
 
 ;;; dired-x 增强的dired功能
 (eval-after-load 'dired-x
