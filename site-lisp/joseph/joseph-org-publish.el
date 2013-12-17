fc9b25f8ca018bdd8e57e35e2ac4e523ccfc1f8a
jixiuf <jixiuf@gmail.com>
2013-12-18 00:45:29 +0800

    (require 'org-html nil t)
    受影响的文件:
        joseph-org-publish.el

--- site-lisp/joseph/joseph-org-publish.el
+++ site-lisp/joseph/joseph-org-publish.el
@@ -3,7 +3,7 @@
     (add-to-list 'load-path  (expand-file-name "."))
     (require 'joseph_byte_compile_include)
     (require 'org)
-    (require 'org-html)
+    (require 'org-html nil t)
     (require 'yasnippet)
     (require 'joseph-outline-lazy)
   )
