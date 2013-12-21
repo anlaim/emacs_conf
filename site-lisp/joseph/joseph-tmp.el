1a0c26e39402bb55828a1d3edc9009db941a3b4f
jixiuf <jixiuf@gmail.com>
2013-12-22 02:17:26 +0800

    加一些经常修改的代码模版到joseph-tmp
    受影响的文件:
        joseph-tmp.el

--- site-lisp/joseph/joseph-tmp.el
+++ site-lisp/joseph/joseph-tmp.el
@@ -1,6 +1,29 @@
+;; (setq helm-gtags-tag-location-alist
+;;       '(
+;;         ;; (c-mode  "/usr/include/" "/usr/kernel/")
+;;         (c++-mode  "/Volumes/data/repos/opencd/opencv-2.4.6.1/")))
 
+;; (setq-default ac-clang-flags
+;;               (mapcar (lambda (item)(concat "-I" item))
+;;                       (split-string
+;;                        "usr/include/c++/4.2.1/
+;;                             usr/include/c++/4.2.1/backward
+;;                             usr/include/c++/4.2.1/bits
+;;                             usr/include/c++/4.2.1/debug
+;;                             usr/include/c++/4.2.1/ext
+;;                             usr/include/c++/4.2.1/tr1")))
 
+(when (equal system-type 'windows-nt)
+  ;; do sth
+  nil)
 
+(when (equal system-type 'darwin)
+  ;; do sth
+  nil)
+
+(when (equal system-type 'gnu/linux)
+  ;; do sth
+  nil)
 
 (provide 'joseph-tmp)
 
