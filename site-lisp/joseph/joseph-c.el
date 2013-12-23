f3ec2cd872fd304fd0f0ce4be566a196fe6c2139
jixiuf <jixiuf@gmail.com>
2013-12-23 13:54:40 +0800

    删除没用的注释，  joseph-c , 去掉 ac-semantic source
    受影响的文件:
        joseph-c.el

--- site-lisp/joseph/joseph-c.el
+++ site-lisp/joseph/joseph-c.el
@@ -1,62 +1,4 @@
-;;; joseph-c.el --- config for c    -*- coding:utf-8 -*-
-
-;; Description: config for c
-;; Created: 2011-10-22 12:05
-;; Last Updated: 纪秀峰 2013-01-22 13:37:25 星期二
-;; Author: 纪秀峰  jixiuf@gmail.com
-;; Maintainer:  纪秀峰  jixiuf@gmail.com
-;; Keywords: c
-;; URL: http://www.emacswiki.org/emacs/joseph-c.el
-
-;; Copyright (C) 2011, 纪秀峰, all rights reserved.
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;;
-
-;;; Commands:
-;;
-;; Below are complete command list:
-;;
-;;
-;;; Customizable Options:
-;;
-;; Below are customizable option list:
-;;
-
-;;; Code:
-;;; byte
-(eval-when-compile
-  (add-to-list 'load-path  (expand-file-name "."))
-  (require 'joseph_byte_compile_include)
-  (require 'gdb-mi)
-  (require 'joseph-util)
-  )
-
 ;;; config
-
-(define-key-lazy c-mode-base-map ";" 'joseph-append-semicolon-at-eol  'cc-mode)
-
-(eval-after-load 'cc-mode '(progn
-                             (require 'joseph-cedet)
-                             (add-hook 'c-mode-hook '(lambda ()
-                                                       ;; (semantic-mode t)
-                                                       (setq ac-sources (append '(ac-source-semantic) ac-sources))))
-                             ))
-
 
 ;; (require 'gdb-ui)
 
@@ -69,7 +11,7 @@
 ;;  :ignore-case t
 ;;  :doc-spec '(("(libc)Symbol Index" nil nil nil)))
 
-(setq gdb-many-windows t)
+(setq-default gdb-many-windows t)
 ;; (global-set-key [f6] 'gud-step)
 ;; (global-set-key [f7] 'gud-next)
 ;; (global-set-key [f8] 'gud-finish)
