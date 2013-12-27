63110260d8f8d2e01bd657f0aaff56fcc5753a58
jixiuf <jixiuf@gmail.com>
2013-12-27 12:12:54 +0800

    ac-clang config

--- site-lisp/joseph/joseph-auto-complete.el
+++ site-lisp/joseph/joseph-auto-complete.el
@@ -86,39 +86,6 @@
   ;;set ac-clang-executable if clang is not under PATH
   (when (and (featurep 'auto-complete-clang)
              ac-clang-executable)       ;只有当clang确实在系统上存在时,才启用
-    (if (equal system-type 'windows-nt)
-      (setq-default ac-clang-flags
-                    (mapcar (lambda (item)(concat "-I" item))
-                            (split-string
-                             ;; 或者如果没装mingw的g++
-                             ;;也可以用vs下的include
-                             ;; D:/usr/vs2010/VC/include
-                             ;; 似乎w32上的clang 必须装vs2010才能运行
-                             "
-                 d:/usr/mingw/include/
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/backward
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/bits
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/debug
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/decimal
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/ext
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/mingw32
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/parallel
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/profile
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/tr1
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/tr2
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include-fixed/
-                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/")))
-      (setq-default ac-clang-flags
-                    (mapcar (lambda (item)(concat "-I" item))
-                            (split-string
-                             "usr/include/
-                              usr/include/c++/4.2.1/
-                              usr/include/c++/4.2.1/backward
-                              usr/include/c++/4.2.1/bits
-                              usr/include/c++/4.2.1/debug
-                              usr/include/c++/4.2.1/ext
-                              usr/include/c++/4.2.1/tr1"))))
     (add-to-list 'ac-sources 'ac-source-clang)))
 
 (add-hook 'c++-mode-hook 'my-ac-clang-setup)
