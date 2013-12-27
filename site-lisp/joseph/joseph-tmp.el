d041af1583babf30e36094d5f8c353cc78fa2d2e
jixiuf <jixiuf@gmail.com>
2013-12-27 12:11:50 +0800

    tmp config

--- site-lisp/joseph/joseph-tmp.el
+++ site-lisp/joseph/joseph-tmp.el
@@ -4,6 +4,29 @@
           ;; (c-mode  "/usr/include/" "/usr/kernel/")
           (c++-mode
            "d:/usr/mingw/include/")))
+  (setq-default ac-clang-flags
+                (mapcar (lambda (item)(concat "-I" item))
+                        (split-string
+                         ;; 或者如果没装mingw的g++
+                         ;;也可以用vs下的include
+                         ;; D:/usr/vs2010/VC/include
+                         ;; 似乎w32上的clang 必须装vs2010才能运行
+                         "
+                 d:/usr/mingw/include/
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/backward
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/bits
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/debug
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/decimal
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/ext
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/mingw32
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/parallel
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/profile
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/tr1
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/c++/tr2
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include-fixed/
+                 d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/")))
+
 
 
   ;; do sth
