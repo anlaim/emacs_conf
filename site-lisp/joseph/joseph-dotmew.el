fcae9bb00753015213f709e1a33a14e7b8a5e2f3
jixiuf <jixiuf@gmail.com>
2014-01-01 21:44:38 +0800

    mew config

--- site-lisp/joseph/joseph-dotmew.el
+++ site-lisp/joseph/joseph-dotmew.el
@@ -22,22 +22,22 @@
 (setq mew-prog-pgp "gpg")
 
 ;; 使用mew 显示html格式文档
-(require 'mew-w3m nil t)
-(setq mew-prog-html '(mew-mime-text/html-w3m nil nil))
-(setq mew-mime-multipart-alternative-list '("Text/Html" "Text/Plan" ".*"))
-(define-key mew-summary-mode-map "T" 'mew-w3m-view-inline-image)
-(setq mew-use-w3m-minor-mode t)
-(add-hook 'mew-message-hook 'mew-w3m-minor-mode-setter)
-;; Press "T":    Toggle the visibility of the images included its message only.
-;; Press "C-uT": Display the all images included its Text/Html part."
-(define-key mew-summary-mode-map "T" 'mew-w3m-view-inline-image)
-;; (4) You can use emacs-w3m to fetch and/or browse
-;; `external-body with URL access'. To activate this feaeture,
-;; add followings also:
-;;
-(setq mew-ext-url-alist
-     '(("^application/" "Fetch by emacs-w3m" mew-w3m-ext-url-fetch nil)
-       (t "Browse by emacs-w3m" mew-w3m-ext-url-show nil)))
+(when (require 'mew-w3m nil t)
+  (setq mew-prog-html '(mew-mime-text/html-w3m nil nil))
+  (setq mew-mime-multipart-alternative-list '("Text/Html" "Text/Plan" ".*"))
+  (define-key mew-summary-mode-map "T" 'mew-w3m-view-inline-image)
+  (setq mew-use-w3m-minor-mode t)
+  (add-hook 'mew-message-hook 'mew-w3m-minor-mode-setter)
+  ;; Press "T":    Toggle the visibility of the images included its message only.
+  ;; Press "C-uT": Display the all images included its Text/Html part."
+  (define-key mew-summary-mode-map "T" 'mew-w3m-view-inline-image)
+  ;; (4) You can use emacs-w3m to fetch and/or browse
+  ;; `external-body with URL access'. To activate this feaeture,
+  ;; add followings also:
+  ;;
+  (setq mew-ext-url-alist
+        '(("^application/" "Fetch by emacs-w3m" mew-w3m-ext-url-fetch nil)
+          (t "Browse by emacs-w3m" mew-w3m-ext-url-show nil))))
 
 
 ;; 我们分别定义了两个 folder，”default”，”pop3-gmail-folder" 如果我们切换
