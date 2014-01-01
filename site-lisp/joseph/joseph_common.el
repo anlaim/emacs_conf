33e857c6b0ad4cd0c870a8db6a396f75516fb36b
jixiuf <jixiuf@gmail.com>
2014-01-02 00:47:47 +0800

    (setq-default initial-major-mode 'fundamental-mode) ;scratch init mode

--- site-lisp/joseph/joseph_common.el
+++ site-lisp/joseph/joseph_common.el
@@ -15,7 +15,7 @@
 
 (setq-default inhibit-startup-screen t);隐藏启动显示画面
 (setq-default initial-scratch-message nil);关闭scratch消息提示
-;; (setq-default initial-major-mode 'fundamental-mode) ;scratch init mode
+(setq-default initial-major-mode 'fundamental-mode) ;scratch init mode
 
 (setq-default use-dialog-box nil  )  ;;不使用对话框进行（是，否 取消） 的选择，而是用minibuffer
 
