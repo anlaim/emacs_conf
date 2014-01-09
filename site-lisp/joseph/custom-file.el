8f2ca34e15cd8ce05dd962fc63042cb93227bdb5
jixiuf <jixiuf@gmail.com>
2014-01-09 20:32:38 +0800

    mod line

--- site-lisp/joseph/custom-file.el
+++ site-lisp/joseph/custom-file.el
@@ -107,6 +107,33 @@
                   (foreground-color . "green")
                   )))
 
+(setq-default undo-tree-mode-lighter " Ü") ;undo
+(setq-default helm-completion-mode-string " H") ;undo
+
+(setq-default mode-line-cleaner-alist
+              `((auto-complete-mode . " á")
+                (yas-minor-mode . " ý")
+                (undo-tree-mode . " Út")
+                (golden-ratio-mode . "")
+                ;; major mode
+                (fundamental-mode . "Fd")
+                (ibuffer-mode . "iBuf")
+                (lisp-interaction-mode . "λ")
+                (emacs-lisp-mode . "EL")))
+
+(defun clean-mode-line ()
+  (interactive)
+  (dolist (cleaner mode-line-cleaner-alist)
+    (let* ((mode (car cleaner))
+           (mode-str (cdr cleaner))
+           (old-mode-str (cdr (assq mode minor-mode-alist))))
+      (when old-mode-str
+        (setcar old-mode-str mode-str))
+      ;; major mode
+      (when (eq mode major-mode)
+        (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
 
 (require 'joseph-font)
 
