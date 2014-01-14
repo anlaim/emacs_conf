c2cdc6370cf68d0bb65fbbdbaaa34b4251daf7d7
jixiuf <jixiuf@gmail.com>
2014-01-14 10:20:47 +0800

    tmp w32 config

--- site-lisp/joseph/joseph-tmp-w32.el
+++ site-lisp/joseph/joseph-tmp-w32.el
@@ -1,18 +1,23 @@
 ;;; Code:
-(setq helm-gtags-tag-location-alist
-      '(
-        ;; (c-mode  "/usr/include/" "/usr/kernel/")
-        (c++-mode
-         "d:/usr/mingw/include/")))
-
-(setq-default ac-clang-flags
-              (mapcar (lambda (item)(concat "-I" item))
-                      (split-string
-                       ;; 或者如果没装mingw的g++
-                       ;;也可以用vs下的include
-                       ;; D:/usr/vs2010/VC/include
-                       ;; 似乎w32上的clang 必须装vs2010才能运行
-                       "../
+(when (equal system-type 'windows-nt)
+  (setq helm-gtags-tag-location-alist
+        '(
+          ;; (c-mode  "/usr/include/" "/usr/kernel/")
+          (c++-mode
+           "D:/usr/opencv/build/include/"
+           "D:/usr/opencv/include/"
+           "d:/usr/mingw/include/")))
+  (setq-default ac-clang-flags
+                (mapcar (lambda (item)(concat "-I" item))
+                        (split-string
+                         ;; 或者如果没装mingw的g++
+                         ;;也可以用vs下的include
+                         ;; D:/usr/vs2010/VC/include
+                         ;; 似乎w32上的clang 必须装vs2010才能运行
+                         "
+           D:/usr/opencv/build/include/
+           D:/usr/opencv/include/
+../
                  ./
                  ../include/
                  d:/usr/mingw/include/
@@ -30,10 +35,14 @@
                  d:/usr/mingw/lib/gcc/mingw32/4.8.1/include-fixed/
                  d:/usr/mingw/lib/gcc/mingw32/4.8.1/include/")))
 
-(setq flymake-cc-additional-compilation-flags ;flymake for c++ c
-      (mapcar (lambda (item)(concat "-I" item))
-              (split-string
-               "./ ../ ../include/ ./include/")))
+  (setq flymake-cc-additional-compilation-flags ;flymake for c++ c
+        (mapcar (lambda (item)(concat "-I" item))
+                (split-string
+                 "D:/usr/opencv/build/include/
+           D:/usr/opencv/include/
+./ ../ ../include/ ./include/")))
+  ;; do sth
+  nil)
 
 (provide 'joseph-tmp-w32)
 
