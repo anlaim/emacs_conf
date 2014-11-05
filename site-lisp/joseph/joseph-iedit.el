53b32ee66850d48cdad99f274fae571a04bb45a3
jixiuf <jixiuf@gmail.com>
2014-11-05 17:06:04 +0800

    iedit C-M-l,support console emacs now

--- site-lisp/joseph/joseph-iedit.el
+++ site-lisp/joseph/joseph-iedit.el
@@ -1,7 +1,7 @@
 ;; (autoload 'iedit-mode "iedit" "Edit current search matches")
 (eval-when-compile (require 'iedit))
 
-(setq-default iedit-toggle-key-default (kbd "C-M-;"))
+(setq-default iedit-toggle-key-default (kbd "C-M-l"))
 (autoload 'iedit-mode-from-isearch "iedit" "enable iedit-mode when in isearch mode")
 (define-key global-map iedit-toggle-key-default 'iedit-mode)
 (define-key isearch-mode-map iedit-toggle-key-default 'iedit-mode-from-isearch)
