5213ef9a8261678044e55fd25debc5519f1e7d23
jixiuf <jixiuf@gmail.com>
2014-11-07 23:13:41 +0800

    remove default face

--- site-lisp/joseph/custom-file.el
+++ site-lisp/joseph/custom-file.el
@@ -155,7 +155,7 @@ kana:-*-Microsoft YaHei-normal-normal-normal-*-14-*-*-*-p-0-iso10646-1")
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "#0C1021" :foreground "#F8F8F8"))))
+
  '(blue ((t (:foreground "blue"))))
  '(buffers-tab ((t (:background "#0C1021" :foreground "#F8F8F8"))))
  '(completions-common-part ((t (:inherit default :foreground "Cyan"))))
