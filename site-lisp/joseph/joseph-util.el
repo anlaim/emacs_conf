4fad67d3ecdc1fde1333324af79b9aa89bce70ce
jixiuf <jixiuf@gmail.com>
2014-11-08 21:34:33 +0800

    remove unused code

--- site-lisp/joseph/joseph-util.el
+++ site-lisp/joseph/joseph-util.el
@@ -105,25 +105,25 @@
 ;; `C-cxyzz'插入两个"hello-world"
 ;; `C-cxyzzz'插入三个"hello-world"
 ;; http://stackoverflow.com/questions/7560094/two-key-shortcut-in-emacs-without-repressing-the-first-key
-(defmacro with-easy-repeat (&rest body)
-  "Execute BODY and repeat while the user presses the last key."
-  (declare (indent 0))
-  `(let* ((repeat-key (and (> (length (this-single-command-keys)) 1)
-                           last-input-event))
-          (repeat-key-str (format-kbd-macro (vector repeat-key) nil)))
-     ,@body
-     (while repeat-key
-       (message "(Type %s to repeat)" repeat-key-str)
-       (let ((event (read-event)))
-         (clear-this-command-keys t)
-         (if (equal event repeat-key)
-             (progn ,@body
-                    (setq last-input-event nil))
-           (setq repeat-key nil)
-           (push last-input-event unread-command-events))))))
+;; (defmacro with-easy-repeat (&rest body)
+;;   "Execute BODY and repeat while the user presses the last key."
+;;   (declare (indent 0))
+;;   `(let* ((repeat-key (and (> (length (this-single-command-keys)) 1)
+;;                            last-input-event))
+;;           (repeat-key-str (format-kbd-macro (vector repeat-key) nil)))
+;;      ,@body
+;;      (while repeat-key
+;;        (message "(Type %s to repeat)" repeat-key-str)
+;;        (let ((event (read-event)))
+;;          (clear-this-command-keys t)
+;;          (if (equal event repeat-key)
+;;              (progn ,@body
+;;                     (setq last-input-event nil))
+;;            (setq repeat-key nil)
+;;            (push last-input-event unread-command-events))))))
 
 ;; (join "\\|" "regexp1" "regexp2" )
-(defmacro join (join-char &rest others) `(mapconcat 'identity ',others ,join-char))
+;; (defmacro join (join-char &rest others) `(mapconcat 'identity ',others ,join-char))
 
 (provide 'joseph-util)
 ;;; joseph-util.el ends here
