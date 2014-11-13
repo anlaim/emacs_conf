37b555af446188f153152339e858926c92ae11bf
jixiuf <jixiuf@gmail.com>
2014-11-13 14:39:53 +0800

    recentf-max-saved-items 500

--- site-lisp/joseph/joseph_common.el
+++ site-lisp/joseph/joseph_common.el
@@ -128,7 +128,7 @@
  recentf-save-file "~/.emacs.d/cache/recentf"
  ;;匹配这些表达示的文件，不会被加入到最近打开的文件中
  recentf-exclude  `("\\.elc$" ,(regexp-quote (expand-file-name "~/.emacs.d/cache/"))  "^/tmp/"  "/TAGS$" "java_base.tag" ".erlang.cookie" "xhtml-loader.rnc" "COMMIT_EDITMSG")
- recentf-max-saved-items 50
+ recentf-max-saved-items 500
  ring-bell-function 'ignore
  
  initial-buffer-choice t
