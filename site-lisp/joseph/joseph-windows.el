99bc9565eae193762eafa4f695675ed6c910ff4d
jixiuf <jixiuf@gmail.com>
2014-11-19 00:43:05 +0800

    comment window keybind

--- site-lisp/joseph/joseph-windows.el
+++ site-lisp/joseph/joseph-windows.el
@@ -23,13 +23,6 @@
 ;; ;shift + 上下左右键，在各窗口间跳转。
 ;; (windmove-default-keybindings 'shift)
 
-(global-set-key "\C-x2" 'split-window-func-with-other-buffer-vertically )
-(global-set-key "\C-x3"  'split-window-func-with-other-buffer-horizontally)
-(global-set-key "\C-z2" 'split-window-func-with-other-buffer-vertically )
-(global-set-key "\C-z3"  'split-window-func-with-other-buffer-horizontally)
-(global-set-key "\C-z1"  'delete-other-windows)
-(global-set-key "\C-z0"  'delete-window)
-(global-set-key "\C-w3" 'toggle-split-window-horizontally-vertically)
 
 
 ;; ;;; windresize 调整窗口大小。
