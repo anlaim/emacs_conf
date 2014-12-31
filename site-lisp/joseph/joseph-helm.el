00a92998082948fdc4d040cdb77c2c7f342d3b85
jixiuf <jixiuf@gmail.com>
2014-12-31 22:28:57 +0800

    ec =emacsclient

--- site-lisp/joseph/joseph-helm.el
+++ site-lisp/joseph/joseph-helm.el
@@ -2,29 +2,6 @@
   ;; (setq helm-idle-delay 0.3)
   ;; (setq helm-input-idle-delay 0)
 
-(defcustom helm-minibuffer-history-key "M-r"
-  "The key `helm-minibuffer-history' is bound to in minibuffer local maps."
-  :type '(choice (string :tag "Key") (const :tag "no binding"))
-  :group 'helm-config
-  :set
-  (lambda (var key)
-    (cl-dolist (map '(minibuffer-local-completion-map
-                      minibuffer-local-filename-completion-map
-                      minibuffer-local-filename-must-match-map ; Emacs 23.1.+
-                      minibuffer-local-isearch-map
-                      minibuffer-local-map
-                      minibuffer-local-must-match-filename-map ; Older Emacsen
-                      minibuffer-local-must-match-map
-                      minibuffer-local-ns-map))
-      (when (and (boundp map) (keymapp (symbol-value map)))
-        (when (and (boundp var) (symbol-value var))
-          (define-key (symbol-value map)
-              (read-kbd-macro (symbol-value var)) nil))
-        (when key
-          (define-key (symbol-value map)
-              (read-kbd-macro key) 'helm-minibuffer-history))))
-    (set var key)))
-
 (setq-default helm-locate-command (case system-type
                                     ('gnu/linux (expand-file-name "~/.emacs.d/bin/everything.sh %s %s"))
                                     ;; "locate  %s -e -A %s"
