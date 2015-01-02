e0508ae6781f6dcc39c10b48d8001aa5a962f3bb
jixiuf <jixiuf@gmail.com>
2015-01-02 14:54:18 +0800

    (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/submodules/joseph-file-util/"))

--- site-lisp/joseph/joseph_byte_compile_include.el
+++ site-lisp/joseph/joseph_byte_compile_include.el
@@ -1,5 +1,5 @@
  ;; -*-no-byte-compile: t; coding:utf-8  -*-
-(add-to-list 'load-path "~/.emacs.d/site-lisp/submodules/joseph-file-util/")
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/submodules/joseph-file-util/"))
 (require 'joseph-file-util)
 (defvar user-load-path (all-subdir-under-dir-recursively
                         (expand-file-name "~/.emacs.d/site-lisp/") nil nil
