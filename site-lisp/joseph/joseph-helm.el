2a27b33c483cf07f168466440fa054a1a0308977
jixiuf <jixiuf@gmail.com>
2015-01-05 12:45:37 +0800

    joseph helm filelist

--- site-lisp/joseph/joseph-helm.el
+++ site-lisp/joseph/joseph-helm.el
@@ -38,12 +38,12 @@
                                               helm-source-recentf
                                               ;; helm-source-bookmarks
                                               ;; helm-source-file-cache
-                                              helm-source-files-in-current-dir
+                                              ;; helm-source-files-in-current-dir
                                               helm-source-files-in-all-dired
                                               helm-source-joseph-filelist
-                                              helm-source-locate
+                                              ;; helm-source-locate
                                               helm-source-ls-git
-                                              helm-source-mac-spotlight
+                                              ;; helm-source-mac-spotlight
                                               )
               helm-ff-skip-boring-files t
               helm-ff-newfile-prompt-p nil
